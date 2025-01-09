176f911d08185689703b5cd12305a62f5b1af177
chyun <och2510@gmail.com>
2025-01-09 22:06:23 +0900

    chore [#1] 초기 프로젝트 설정
    - 패키지 구조
    - AWS Cloud 연결 정보

---
+++ src/main/java/org/sopt/confeti/ConfetiApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.confeti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfetiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfetiApplication.class, args);
+    }
+
+}
