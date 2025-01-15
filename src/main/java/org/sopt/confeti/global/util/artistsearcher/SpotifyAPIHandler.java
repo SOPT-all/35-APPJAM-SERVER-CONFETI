825327c2474d8177be3877259702518ffd7b45bc
chyun <och2510@gmail.com>
2025-01-16 01:07:00 +0900

    feat [#37] Spotify API Handler 파일 작성
    - Artist Id 목록을 가지고 아티스트 정보를 조회하는 기능

--- src/main/java/org/sopt/confeti/global/util/artistsearcher/SpotifyAPIHandler.java
+++ src/main/java/org/sopt/confeti/global/util/artistsearcher/SpotifyAPIHandler.java
@@ -1,35 +1,20 @@
 package org.sopt.confeti.global.util.artistsearcher;
 
-import com.neovisionaries.i18n.CountryCode;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.apache.hc.core5.http.ParseException;
-import org.sopt.confeti.annotation.Handler;
-import org.sopt.confeti.global.util.DateConvertor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
-import se.michaelthelin.spotify.model_objects.specification.AlbumSimplified;
 import se.michaelthelin.spotify.model_objects.specification.Artist;
-import se.michaelthelin.spotify.model_objects.specification.Paging;
 import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import se.michaelthelin.spotify.requests.data.artists.GetArtistRequest;
 
-@Handler
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Component
 public class SpotifyAPIHandler {
-
-    private static final int ARTIST_LIMIT = 1;
-    private static final int ARTIST_OFFSET = 0;
-    private static final int ALBUM_LIMIT = 10;
-    private static final int ALBUM_OFFSET = 0;
 
     @Value("${spotify.credentials.client-id}")
     private String clientId;
@@ -39,87 +24,24 @@ public class SpotifyAPIHandler {
 
     private SpotifyApi spotifyApi;
 
-    private final DateConvertor dateConvertor;
+    private GetArtistRequest getArtistRequest;
 
-    public void init() {
+    public SpotifyAPIHandler() {
         createSpotifyApi();
         generateAccessToken();
     }
 
-    public Optional<ConfetiArtist> findArtistsByKeyword(final String keyword) {
-        Optional<Artist> searchedArtist = searchArtistByKeyword(keyword);
+    public List<ConfetiArtist> findArtistsByArtistIds(final List<String> artistIds)
+            throws IOException, ParseException, SpotifyWebApiException {
+        Artist[] artists = spotifyApi.getSeveralArtists(
+                artistIds.toArray(new String[0])
+        )
+                .build()
+                .execute();
 
-        if (searchedArtist.isEmpty()) {
-            return Optional.empty();
-        }
-
-        Artist artist = searchedArtist.get();
-
-        return Optional.of(
-                ConfetiArtist.toConfetiArtist(artist, findLatestReleaseAt(keyword, artist))
-        );
-    }
-
-    public List<ConfetiArtist> findArtistsByArtistIds(final List<String> artistIds) {
-        try {
-            Artist[] artists = spotifyApi.getSeveralArtists(
-                            artistIds.toArray(new String[0])
-                    )
-                    .build()
-                    .execute();
-
-            return Arrays.stream(artists)
-                    .map(ConfetiArtist::toConfetiArtist)
-                    .toList();
-        } catch (IOException | ParseException | SpotifyWebApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Optional<Artist> searchArtistByKeyword(final String keyword) {
-        try {
-            Paging<Artist> artists = spotifyApi.searchArtists(keyword)
-                    .market(CountryCode.KR)
-                    .limit(ARTIST_LIMIT)
-                    .offset(ARTIST_OFFSET)
-                    .build()
-                    .execute();
-
-            return Arrays.stream(artists.getItems())
-                    .findFirst();
-        } catch (IOException | ParseException | SpotifyWebApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private LocalDate findLatestReleaseAt(final String keyword, final Artist artist) {
-        Paging<AlbumSimplified> albums = searchAlbumByKeyword(keyword);
-
-        return Arrays.stream(albums.getItems())
-                .filter((searchedAlbum) ->
-                        Arrays.stream(searchedAlbum.getArtists())
-                                .anyMatch((searchedArtist) ->
-                                        searchedArtist.getId().equals(artist.getId())
-                                )
-                )
-                .max(Comparator.comparing(AlbumSimplified::getReleaseDate))
-                .map(
-                        albumSimplified -> dateConvertor.convertToLocalDate(albumSimplified.getReleaseDate())
-                )
-                .get();
-    }
-
-    private Paging<AlbumSimplified> searchAlbumByKeyword(final String keyword) {
-        try {
-            return spotifyApi.searchAlbums(keyword)
-                    .market(CountryCode.KR)
-                    .limit(ALBUM_LIMIT)
-                    .offset(ALBUM_OFFSET)
-                    .build()
-                    .execute();
-        } catch (IOException | ParseException | SpotifyWebApiException e) {
-            throw new RuntimeException(e);
-        }
+        return Arrays.stream(artists)
+                .map(ConfetiArtist::toConfetiArtist)
+                .toList();
     }
 
     private void createSpotifyApi() {
