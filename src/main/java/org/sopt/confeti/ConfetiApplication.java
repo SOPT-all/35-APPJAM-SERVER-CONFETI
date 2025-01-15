662f80041513fc71fdb9fd933591e237585ebbb6
chyun <och2510@gmail.com>
2025-01-16 03:35:09 +0900

    feat [#37] SpotifyAPIHandler 초기화를 위해 스프링 빈 생성 이후로 변경

--- src/main/java/org/sopt/confeti/ConfetiApplication.java
+++ src/main/java/org/sopt/confeti/ConfetiApplication.java
@@ -1,13 +1,26 @@
 package org.sopt.confeti;
 
+import lombok.RequiredArgsConstructor;
+import org.sopt.confeti.global.util.artistsearcher.SpotifyAPIHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class ConfetiApplication {
+
+    private final ApplicationContext ac;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfetiApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        SpotifyAPIHandler spotifyAPIHandler = ac.getBean(SpotifyAPIHandler.class);
+        spotifyAPIHandler.init();
+    }
 }
