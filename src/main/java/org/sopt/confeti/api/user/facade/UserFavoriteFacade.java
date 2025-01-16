3cef47729954c33909a64857e07ec336a54d6f1e
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-16 20:16:52 +0900

    feat [#47] Controller, Facade 구현

---
+++ src/main/java/org/sopt/confeti/api/user/facade/UserFavoriteFacade.java
@@ -0,0 +1,33 @@
+package org.sopt.confeti.api.user.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.confeti.annotation.Facade;
+import org.sopt.confeti.domain.festival.Festival;
+import org.sopt.confeti.domain.festival.application.FestivalService;
+import org.sopt.confeti.domain.festivalfavorite.application.FestivalFavoriteService;
+import org.sopt.confeti.domain.user.User;
+import org.sopt.confeti.domain.user.application.UserService;
+import org.springframework.transaction.annotation.Transactional;
+
+@Facade
+@RequiredArgsConstructor
+public class UserFavoriteFacade {
+
+    private final UserService userService;
+    private final FestivalService festivalService;
+    private final FestivalFavoriteService festivalFavoriteService;
+
+    @Transactional
+    public void save(long userId, long festivalId) {
+        User user = userService.findById(userId);
+        Festival festival = festivalService.findByid(festivalId);
+        festivalFavoriteService.save(user, festival);
+    }
+
+    @Transactional
+    public void delete(long userId, long festivalId) {
+        User user = userService.findById(userId);
+        Festival festival = festivalService.findByid(festivalId);
+        festivalFavoriteService.delete(user, festival);
+    }
+}
