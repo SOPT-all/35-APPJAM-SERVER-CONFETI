f9fb4711fb82189dab00c7a5386e9eea70f66d39
chyun <och2510@gmail.com>
2025-01-16 19:54:49 +0900

    feat [#37] @Convertor 어노테이션 추가 및 DateConvertor 구현

---
+++ src/main/java/org/sopt/confeti/annotation/Convertor.java
@@ -0,0 +1,15 @@
+package org.sopt.confeti.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Convertor {
+}
---
+++ src/main/java/org/sopt/confeti/global/util/DateConvertor.java
@@ -0,0 +1,39 @@
+package org.sopt.confeti.global.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import org.sopt.confeti.annotation.Convertor;
+
+@Convertor
+public class DateConvertor {
+
+    private final DateTimeFormatter localDateFormat = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+    private final DateTimeFormatter localTimeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private final DateTimeFormatter localDateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+    public String convert(final LocalDate localDate) {
+        return localDate.format(localDateFormat);
+    }
+
+    public String convert(final LocalTime localTime) {
+        return localTime.format(localTimeFormat);
+    }
+
+    public String convert(final LocalDateTime localDateTime) {
+        return localDateTime.format(localDateTimeFormat);
+    }
+
+    public LocalDate convertToLocalDate(final String localDate) {
+        return LocalDate.parse(localDate, localDateFormat);
+    }
+
+    public LocalTime convertToLocalTime(final String localTime) {
+        return LocalTime.parse(localTime, localTimeFormat);
+    }
+
+    public LocalDateTime convertToLocalDateTime(final String localDateTime) {
+        return LocalDateTime.parse(localDateTime, localDateTimeFormat);
+    }
+}
