bf6028d756e4ad036c8a72a5403a4e2c88ee556f
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-16 20:16:52 +0900

    feat [#47] Controller, Facade 구현

---
+++ src/main/java/org/sopt/confeti/api/user/controller/UserFavoriteController.java
@@ -0,0 +1,33 @@
+package org.sopt.confeti.api.user.controller;
+
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import org.sopt.confeti.api.user.facade.UserFavoriteFacade;
+import org.sopt.confeti.global.common.BaseResponse;
+import org.sopt.confeti.global.message.SuccessMessage;
+import org.sopt.confeti.global.util.ApiResponseUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/user/favorites")
+public class UserFavoriteController {
+
+    private final UserFavoriteFacade userFavoriteFacade;
+
+    @PostMapping("/festivals/{festivalId}")
+    public ResponseEntity<BaseResponse<?>> postFavoriteFestival(@RequestHeader("Authorization") Long userId, @PathVariable
+                                                                @Min(value = 0, message = "요청 형식이 올바르지 않습니다.") Long festivalId) {
+        userFavoriteFacade.save(userId, festivalId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS);
+    }
+
+    @DeleteMapping("/festivals/{festivalId}")
+    public ResponseEntity<BaseResponse<?>> deleteFavoriteFestival(@RequestHeader("Authorization") Long userId, @PathVariable
+                                                                  @Min(value = 0, message = "요청 형식이 올바르지 않습니다.") Long festivalId) {
+        userFavoriteFacade.delete(userId, festivalId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS);
+    }}
