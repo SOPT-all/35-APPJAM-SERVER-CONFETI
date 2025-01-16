ded27bdcd711720bf5bf768051e32f727f41136d
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-16 20:17:56 +0900

    feat [#47] Repositiory 구현

--- src/main/java/org/sopt/confeti/domain/festivalfavorite/infra/repository/FestivalFavoriteRepository.java
+++ src/main/java/org/sopt/confeti/domain/festivalfavorite/infra/repository/FestivalFavoriteRepository.java
@@ -1,7 +1,12 @@
 package org.sopt.confeti.domain.festivalfavorite.infra.repository;
 
+import org.sopt.confeti.domain.festival.Festival;
 import org.sopt.confeti.domain.festivalfavorite.FestivalFavorite;
+import org.sopt.confeti.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FestivalFavoriteRepository extends JpaRepository<FestivalFavorite, Long> {
+    Optional<FestivalFavorite> findByUserIdAndFestivalId(long userId, long festivalId);
 }
