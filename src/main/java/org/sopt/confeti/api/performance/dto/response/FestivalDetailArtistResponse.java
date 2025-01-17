046c1627fed116ccb01dac2b957649ea462532c2
chyun <och2510@gmail.com>
2025-01-17 23:12:34 +0900

    feat [#67] 페스티벌 정보 조회 API 구현

--- src/main/java/org/sopt/confeti/api/performance/dto/response/FestivalDetailArtistResponse.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/response/FestivalDetailArtistResponse.java
@@ -1,8 +1,17 @@
 package org.sopt.confeti.api.performance.dto.response;
 
+import org.sopt.confeti.api.performance.facade.dto.response.FestivalDetailArtistDTO;
+
 public record FestivalDetailArtistResponse(
         String artistId,
         String name,
         String profileUrl
 ) {
+    public static FestivalDetailArtistResponse from(final FestivalDetailArtistDTO artist) {
+        return new FestivalDetailArtistResponse(
+                artist.artistId(),
+                artist.name(),
+                artist.profileUrl()
+        );
+    }
 }
