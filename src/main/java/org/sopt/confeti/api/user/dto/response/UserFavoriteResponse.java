c391ec443dc440221afeb705b413173a804e3575
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-18 04:34:28 +0900

    fix [#57] 메서드 of->from 변경

--- src/main/java/org/sopt/confeti/api/user/dto/response/UserFavoriteResponse.java
+++ src/main/java/org/sopt/confeti/api/user/dto/response/UserFavoriteResponse.java
@@ -5,7 +5,7 @@ import org.sopt.confeti.api.user.facade.dto.response.UserFavoriteArtistListDTO;
 import java.util.List;
 
 public record UserFavoriteResponse(List<UserFavoriteListResponse> artists) {
-    public static UserFavoriteResponse of(final List<UserFavoriteArtistListDTO> artistListDTO) {
+    public static UserFavoriteResponse from(final List<UserFavoriteArtistListDTO> artistListDTO) {
         return new UserFavoriteResponse(
                 artistListDTO.stream()
                         .map(UserFavoriteListResponse::from)
