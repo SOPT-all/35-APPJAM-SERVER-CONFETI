6c423ed7c2a6a58995570dc703f7dd85ab204d23
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-18 01:24:39 +0900

    feat [#57] ArtistFavoriteService, Repository 구현

---
+++ src/main/java/org/sopt/confeti/domain/artistfavorite/application/ArtistFavoriteService.java
@@ -0,0 +1,26 @@
+package org.sopt.confeti.domain.artistfavorite.application;
+
+import lombok.AllArgsConstructor;
+import org.sopt.confeti.domain.artistfavorite.ArtistFavorite;
+import org.sopt.confeti.domain.artistfavorite.infra.repository.ArtistFavoriteRepository;
+import org.sopt.confeti.domain.user.User;
+import org.sopt.confeti.global.util.artistsearcher.ArtistResolver;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ArtistFavoriteService {
+    ArtistFavoriteRepository artistFavoriteRepository;
+    private final ArtistResolver artistResolver;
+
+    @Transactional(readOnly = true)
+    public List<ArtistFavorite> getArtistList(User user) {
+        List<ArtistFavorite> artistList = artistFavoriteRepository.findAllByUserId(user.getId());
+        artistResolver.load(artistList);
+
+        return artistList;
+    }
+}
