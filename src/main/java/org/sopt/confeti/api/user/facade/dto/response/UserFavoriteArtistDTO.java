9c974d4948b0109f2d55378cefecc489d3ad4330
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-18 01:24:16 +0900

    feat [#57] UserFavoriteFacade 구현

---
+++ src/main/java/org/sopt/confeti/api/user/facade/dto/response/UserFavoriteArtistDTO.java
@@ -0,0 +1,15 @@
+package org.sopt.confeti.api.user.facade.dto.response;
+
+import org.sopt.confeti.domain.artistfavorite.ArtistFavorite;
+
+import java.util.List;
+
+public record UserFavoriteArtistDTO (List<UserFavoriteArtistListDTO> artists) {
+    public static UserFavoriteArtistDTO of(final List<ArtistFavorite> artists) {
+        return new UserFavoriteArtistDTO(
+                artists.stream()
+                        .map(UserFavoriteArtistListDTO::from)
+                        .toList()
+        );
+    }
+}
