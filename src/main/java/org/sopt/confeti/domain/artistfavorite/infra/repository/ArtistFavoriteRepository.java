894fdfd5c8d1e9597ba92d9dc056a12e55af8726
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-18 01:24:39 +0900

    feat [#57] ArtistFavoriteService, Repository 구현

--- src/main/java/org/sopt/confeti/domain/artistfavorite/infra/repository/ArtistFavoriteRepository.java
+++ src/main/java/org/sopt/confeti/domain/artistfavorite/infra/repository/ArtistFavoriteRepository.java
@@ -3,5 +3,8 @@ package org.sopt.confeti.domain.artistfavorite.infra.repository;
 import org.sopt.confeti.domain.artistfavorite.ArtistFavorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArtistFavoriteRepository extends JpaRepository<ArtistFavorite, Long> {
+    List<ArtistFavorite> findAllByUserId(Long userId);
 }
