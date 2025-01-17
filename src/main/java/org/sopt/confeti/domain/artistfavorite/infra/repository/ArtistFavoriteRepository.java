52d0ccb8f357d9cf43d5ed57dc71a7a3fefe8dcc
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-18 02:39:22 +0900

    feat [#57] 아티스트 목록 최대 3개만 조회하도록 제한

--- src/main/java/org/sopt/confeti/domain/artistfavorite/infra/repository/ArtistFavoriteRepository.java
+++ src/main/java/org/sopt/confeti/domain/artistfavorite/infra/repository/ArtistFavoriteRepository.java
@@ -2,9 +2,12 @@ package org.sopt.confeti.domain.artistfavorite.infra.repository;
 
 import org.sopt.confeti.domain.artistfavorite.ArtistFavorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ArtistFavoriteRepository extends JpaRepository<ArtistFavorite, Long> {
-    List<ArtistFavorite> findAllByUserId(Long userId);
+    @Query(value = "select * from artist_favorites where user_id = :userId order by rand() limit 3", nativeQuery = true)
+    List<ArtistFavorite> findTop3ByUserIdOrderByRand(@Param("userId") Long userId);
 }
