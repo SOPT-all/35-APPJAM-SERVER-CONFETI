c569d4b73349b1b9703918d025eca4d78596a98f
chyun <och2510@gmail.com>
2025-01-18 22:53:59 +0900

    refactor [#105] 날짜 매핑 방법 변경

--- src/main/java/org/sopt/confeti/api/performance/dto/request/CreateFestivalTimeRequest.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/request/CreateFestivalTimeRequest.java
@@ -3,9 +3,12 @@ package org.sopt.confeti.api.performance.dto.request;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public record CreateFestivalTimeRequest(
+        @DateTimeFormat(pattern = "HH:mm:ss")
         LocalTime startAt,
+        @DateTimeFormat(pattern = "HH:mm:ss")
         LocalTime endAt,
         List<CreateFestivalArtistRequest> festivalArtists
 ) {
--- src/main/java/org/sopt/confeti/api/user/dto/response/UserInfoResponse.java
+++ src/main/java/org/sopt/confeti/api/user/dto/response/UserInfoResponse.java
@@ -2,7 +2,11 @@ package org.sopt.confeti.api.user.dto.response;
 
 import org.sopt.confeti.api.user.facade.dto.response.UserInfoDTO;
 
-public record UserInfoResponse (Long userId, String profileUrl, String username){
+public record UserInfoResponse (
+        Long userId,
+        String profileUrl,
+        String username
+) {
     public static UserInfoResponse from (UserInfoDTO userInfoDTO) {
         return new UserInfoResponse(userInfoDTO.userId(), userInfoDTO.profileUrl(), userInfoDTO.username());
     }
