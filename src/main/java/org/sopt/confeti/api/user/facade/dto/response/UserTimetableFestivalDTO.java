6e0594d03d417f770ead8e4818a8ea5d7325910c
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-18 19:24:23 +0900

    feat [#83] UserTimetable Dto 구현

---
+++ src/main/java/org/sopt/confeti/api/user/facade/dto/response/UserTimetableFestivalDTO.java
@@ -0,0 +1,24 @@
+package org.sopt.confeti.api.user.facade.dto.response;
+
+import org.sopt.confeti.domain.timetablefestival.TimetableFestival;
+
+import java.util.List;
+
+public record UserTimetableFestivalDTO(
+        long festivalId,
+        String title,
+        String logoUrl,
+        List<UserTimetableDatesDTO> festivalDates
+) {
+    public static UserTimetableFestivalDTO from(TimetableFestival timetableFestival) {
+        return new UserTimetableFestivalDTO(
+                timetableFestival.getFestival().getId(),
+                timetableFestival.getFestival().getFestivalTitle(),
+                timetableFestival.getFestival().getFestivalLogoPath(),
+                timetableFestival.getFestival().getDates().stream()
+                        .map(UserTimetableDatesDTO::from)
+                        .toList()
+        );
+    }
+
+}
