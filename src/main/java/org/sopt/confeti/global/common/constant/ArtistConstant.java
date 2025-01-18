91e1e33ba7fd0ce910848956d1d7ed361720fd3a
chyun <och2510@gmail.com>
2025-01-18 19:54:39 +0900

    feat [#92] 아티스트 접기/펼치기 기준 값 상수 추가

---
+++ src/main/java/org/sopt/confeti/global/common/constant/ArtistConstant.java
@@ -0,0 +1,9 @@
+package org.sopt.confeti.global.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ArtistConstant {
+    private static final int ARTIST_BOX_OPEN_CRITERIA = 4;
+}
