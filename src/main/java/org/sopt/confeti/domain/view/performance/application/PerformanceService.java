a23b15282edf142e0a7bd35baa3367d71523ef35
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-21 08:28:16 +0900

    feat [#41] PerformanceService, Repository 구현

--- src/main/java/org/sopt/confeti/domain/view/performance/application/PerformanceService.java
+++ src/main/java/org/sopt/confeti/domain/view/performance/application/PerformanceService.java
@@ -3,6 +3,7 @@ package org.sopt.confeti.domain.view.performance.application;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt.confeti.domain.view.performance.PerformanceDTO;
+import org.sopt.confeti.domain.view.performance.PerformanceTicketDTO;
 import org.sopt.confeti.domain.view.performance.infra.repository.PerformanceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +17,10 @@ public class PerformanceService {
     @Transactional(readOnly = true)
     public List<PerformanceDTO> getFavoritePerformancesPreview(final long userId) {
         return performanceRepository.findFavoritePerformancesPreview(userId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PerformanceTicketDTO> getFavoritePerformancesReservation(final Long userId) {
+        return performanceRepository.findFavoritePerformancesReservation(userId);
     }
 }
