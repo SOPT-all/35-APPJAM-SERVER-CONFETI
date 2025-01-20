4af46d62e82135ee6274d7d8b0cf58270958d963
chyun <och2510@gmail.com>
2025-01-21 03:00:28 +0900

    refactor [#119] 페스티벌 조회 성능 최적화

--- src/main/java/org/sopt/confeti/domain/festival/infra/repository/FestivalRepository.java
+++ src/main/java/org/sopt/confeti/domain/festival/infra/repository/FestivalRepository.java
@@ -3,7 +3,19 @@ package org.sopt.confeti.domain.festival.infra.repository;
 import java.util.List;
 import org.sopt.confeti.domain.festival.Festival;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FestivalRepository extends JpaRepository<Festival, Long> {
-    List<Festival> findByIdIn(final List<Long> festivalIds);
+
+    @Query(value =
+        "SELECT DISTINCT f" +
+                " FROM Festival f" +
+                " JOIN FETCH f.dates fd" +
+                " JOIN FETCH fd.stages fs" +
+                " JOIN FETCH fs.times ft" +
+                " JOIN FETCH ft.artists fa" +
+                " WHERE f.id IN :festivalIds"
+    )
+    List<Festival> findAllByIdIn(final @Param("festivalIds") List<Long> festivalIds);
 }
