62e9fd3b5e2c242cee61615092f8719a2b5aefbb
chyun <och2510@gmail.com>
2025-01-22 06:21:54 +0900

    feat [#126] 커서 베이스 페이징 응답 DTO 생성

--- src/main/java/org/sopt/confeti/api/user/dto/response/TimetablesToAddFestivalResponse.java
+++ src/main/java/org/sopt/confeti/api/user/dto/response/TimetablesToAddFestivalResponse.java
@@ -1,8 +1,18 @@
 package org.sopt.confeti.api.user.dto.response;
 
+import org.sopt.confeti.api.user.facade.dto.response.TimetableToAddDTO;
+import org.sopt.confeti.global.util.S3FileHandler;
+
 public record TimetablesToAddFestivalResponse(
         long festivalId,
         String posterUrl,
         String title
 ) {
+    public static TimetablesToAddFestivalResponse of(final TimetableToAddDTO timetableToAddDTO, final S3FileHandler s3FileHandler) {
+        return new TimetablesToAddFestivalResponse (
+                timetableToAddDTO.festivalId(),
+                s3FileHandler.getFileUrl(timetableToAddDTO.posterPath()),
+                timetableToAddDTO.title()
+        );
+    }
 }
