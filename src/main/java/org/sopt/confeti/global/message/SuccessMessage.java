39889be801b769a202ea9f4c2942834b412d9ed1
chyun <och2510@gmail.com>
2025-01-23 02:40:33 +0900

    feat [#151] CREATED 응답 메세지 추가

--- src/main/java/org/sopt/confeti/global/message/SuccessMessage.java
+++ src/main/java/org/sopt/confeti/global/message/SuccessMessage.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum SuccessMessage {
-    SUCCESS(HttpStatus.OK, "요청이 성공했습니다."),;
+    SUCCESS(HttpStatus.OK, "요청이 성공했습니다."),
+    CREATED(HttpStatus.CREATED, "생성되었습니다."),;
 
     private final HttpStatus httpStatus;
     private final String message;
