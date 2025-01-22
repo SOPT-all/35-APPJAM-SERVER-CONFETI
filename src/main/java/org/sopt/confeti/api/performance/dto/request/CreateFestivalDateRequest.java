8c724becbf7cbb8fd637b2f5f8cc052a906d7663
chyun <och2510@gmail.com>
2025-01-22 23:56:35 +0900

    fix [#145] 페스티벌 생성 요청 형식 수정

--- src/main/java/org/sopt/confeti/api/performance/dto/request/CreateFestivalDateRequest.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/request/CreateFestivalDateRequest.java
@@ -1,15 +1,19 @@
 package org.sopt.confeti.api.performance.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 public record CreateFestivalDateRequest(
         @JsonFormat(pattern = "yyyy.MM.dd")
+        @JsonProperty(value = "festival_at")
         LocalDate festivalAt,
         @JsonFormat(pattern = "HH:mm:ss")
+        @JsonProperty(value = "ticket_open_at")
         LocalTime ticketOpenAt,
+        @JsonProperty(value = "festival_stages")
         List<CreateFestivalStageRequest> festivalStages
 ) {
 }
--- src/main/java/org/sopt/confeti/api/performance/dto/request/CreateFestivalTimeRequest.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/request/CreateFestivalTimeRequest.java
@@ -1,14 +1,18 @@
 package org.sopt.confeti.api.performance.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalTime;
 import java.util.List;
 
 public record CreateFestivalTimeRequest(
         @JsonFormat(pattern = "HH:mm:ss")
+        @JsonProperty(value = "start_at")
         LocalTime startAt,
         @JsonFormat(pattern = "HH:mm:ss")
+        @JsonProperty(value = "end_at")
         LocalTime endAt,
+        @JsonProperty(value = "festival_artists")
         List<CreateFestivalArtistRequest> festivalArtists
 ) {
 }
