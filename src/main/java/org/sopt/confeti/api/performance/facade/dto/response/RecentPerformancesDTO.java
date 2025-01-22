bfdeb79f90c084c34556992af9a83627e85b126e
chyun <och2510@gmail.com>
2025-01-23 06:07:08 +0900

    feat [#136] 파사드 계층에서 컨트롤러 계층 최신 공연 값 DTO 작성

--- src/main/java/org/sopt/confeti/api/performance/facade/dto/response/RecentPerformancesDTO.java
+++ src/main/java/org/sopt/confeti/api/performance/facade/dto/response/RecentPerformancesDTO.java
@@ -1,8 +1,30 @@
 package org.sopt.confeti.api.performance.facade.dto.response;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.sopt.confeti.domain.concert.Concert;
+import org.sopt.confeti.domain.festival.Festival;
 
 public record RecentPerformancesDTO(
         List<RecentPerformanceDTO> performances
 ) {
+    public static RecentPerformancesDTO of(
+            final List<Concert> concerts, final Map<Long, Long> concertMapper,
+            final List<Festival> festivals, final Map<Long, Long> festivalMapper,
+            final int size
+    ) {
+        return new RecentPerformancesDTO(
+                Stream.concat(
+                        concerts.stream()
+                                .map(concert -> RecentPerformanceDTO.of(concert, concertMapper.get(concert.getId()))),
+                        festivals.stream()
+                                .map(festival -> RecentPerformanceDTO.of(festival, festivalMapper.get(festival.getId())))
+                )
+                        .sorted(Comparator.comparing(RecentPerformanceDTO::performanceAt))
+                        .limit(size)
+                        .toList()
+        );
+    }
 }
