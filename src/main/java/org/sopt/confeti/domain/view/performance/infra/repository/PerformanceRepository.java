255b0f62436e3c1e25de405dbf3acce768289ef8
chyun <och2510@gmail.com>
2025-01-23 06:07:58 +0900

    feat [#136] 콘서트/페스티벌 아이디, 공연 타입으로 공연 아이디 조회하는 로직 구현

--- src/main/java/org/sopt/confeti/domain/view/performance/infra/repository/PerformanceRepository.java
+++ src/main/java/org/sopt/confeti/domain/view/performance/infra/repository/PerformanceRepository.java
@@ -3,7 +3,6 @@ package org.sopt.confeti.domain.view.performance.infra.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.sopt.confeti.domain.view.performance.Performance;
-import org.sopt.confeti.global.common.constant.PerformanceType;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
