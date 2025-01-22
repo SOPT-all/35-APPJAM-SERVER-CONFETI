f68200c091c906261ab78731aac7faae39b186b0
chyun <och2510@gmail.com>
2025-01-22 23:56:35 +0900

    fix [#145] 페스티벌 생성 요청 형식 수정

--- src/main/java/org/sopt/confeti/api/performance/dto/request/CreateFestivalRequest.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/request/CreateFestivalRequest.java
@@ -1,29 +1,45 @@
 package org.sopt.confeti.api.performance.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import java.util.List;
 
 public record CreateFestivalRequest(
+        @JsonProperty(value = "festival_title")
         String festivalTitle,
+        @JsonProperty(value = "festival_subtitle")
         String festivalSubtitle,
         @JsonFormat(pattern = "yyyy.MM.dd")
+        @JsonProperty(value = "festival_start_at")
         LocalDate festivalStartAt,
         @JsonFormat(pattern = "yyyy.MM.dd")
+        @JsonProperty(value = "festival_end_at")
         LocalDate festivalEndAt,
+        @JsonProperty(value = "festival_area")
         String festivalArea,
+        @JsonProperty(value = "festival_poster_path")
         String festivalPosterPath,
+        @JsonProperty(value = "festival_poster_bg_path")
         String festivalPosterBgPath,
+        @JsonProperty(value = "festival_info_img_path")
         String festivalInfoImgPath,
+        @JsonProperty(value = "festival_reservation_bg_path")
         String festivalReservationBgPath,
+        @JsonProperty(value = "festival_logo_path")
         String festivalLogoPath,
         @JsonFormat(pattern = "yyyy.MM.dd")
+        @JsonProperty(value = "reserve_at")
         LocalDate reserveAt,
+        @JsonProperty(value = "reservation_url")
         String reservationUrl,
+        @JsonProperty(value = "reservation_office")
         String reservationOffice,
+        @JsonProperty(value = "age_rating")
         String ageRating,
         String time,
         String price,
+        @JsonProperty(value = "festival_dates")
         List<CreateFestivalDateRequest> festivalDates
 ) {
 }
