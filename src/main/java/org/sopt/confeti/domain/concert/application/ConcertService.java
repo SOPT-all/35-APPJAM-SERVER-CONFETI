879acf23fabe9893b191fd5fa7d1d81ad819f6f9
chyun <och2510@gmail.com>
2025-01-23 05:28:18 +0900

    feat [#136] 최신 공연 조회 함수 구현

--- src/main/java/org/sopt/confeti/domain/concert/application/ConcertService.java
+++ src/main/java/org/sopt/confeti/domain/concert/application/ConcertService.java
@@ -1,17 +1,25 @@
 package org.sopt.confeti.domain.concert.application;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt.confeti.domain.concert.Concert;
 import org.sopt.confeti.domain.concert.infra.repository.ConcertRepository;
 import org.sopt.confeti.global.exception.NotFoundException;
 import org.sopt.confeti.global.message.ErrorMessage;
 import org.sopt.confeti.global.util.artistsearcher.ArtistResolver;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ConcertService {
+
+    private static final int INIT_PAGE = 0;
+    private static final String START_AT_COLUMN = "concertStartAt";
 
     private final ConcertRepository concertRepository;
     private final ArtistResolver artistResolver;
@@ -39,5 +47,23 @@ public class ConcertService {
                 .orElseThrow(
                         () -> new NotFoundException(ErrorMessage.NOT_FOUND)
                 );
+    }
+
+    @Transactional(readOnly = true)
+    public List<Concert> getRecentConcerts(final int size) {
+        return concertRepository.findAllByConcertEndAtLessThan(
+                LocalDateTime.now(),
+                getPageRequest(size, getRecentConcertsSort())
+        );
+    }
+
+    private PageRequest getPageRequest(final int size, final Sort sort) {
+        return PageRequest.of(INIT_PAGE, size, sort);
+    }
+
+    private Sort getRecentConcertsSort() {
+        return Sort.by(
+                Order.asc(START_AT_COLUMN)
+        );
     }
 }
