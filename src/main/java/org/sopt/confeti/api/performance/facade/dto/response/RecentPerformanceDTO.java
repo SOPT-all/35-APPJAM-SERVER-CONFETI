7fa093e8b43fb5c2964130d8943f106b4bdd8e58
chyun <och2510@gmail.com>
2025-01-23 06:07:08 +0900

    feat [#136] 파사드 계층에서 컨트롤러 계층 최신 공연 값 DTO 작성

--- src/main/java/org/sopt/confeti/api/performance/facade/dto/response/RecentPerformanceDTO.java
+++ src/main/java/org/sopt/confeti/api/performance/facade/dto/response/RecentPerformanceDTO.java
@@ -1,6 +1,8 @@
 package org.sopt.confeti.api.performance.facade.dto.response;
 
 import java.time.LocalDateTime;
+import org.sopt.confeti.domain.concert.Concert;
+import org.sopt.confeti.domain.festival.Festival;
 import org.sopt.confeti.global.common.constant.PerformanceType;
 
 public record RecentPerformanceDTO(
@@ -12,4 +14,27 @@ public record RecentPerformanceDTO(
         LocalDateTime performanceAt,
         String posterPath
 ) {
+    public static RecentPerformanceDTO of(final Concert concert, final long performanceId) {
+        return new RecentPerformanceDTO(
+                performanceId,
+                concert.getId(),
+                PerformanceType.CONCERT,
+                concert.getConcertTitle(),
+                concert.getConcertSubtitle(),
+                concert.getConcertStartAt(),
+                concert.getConcertPosterPath()
+        );
+    }
+
+    public static RecentPerformanceDTO of(final Festival festival, final long performanceId) {
+        return new RecentPerformanceDTO(
+                performanceId,
+                festival.getId(),
+                PerformanceType.CONCERT,
+                festival.getFestivalTitle(),
+                festival.getFestivalSubtitle(),
+                festival.getFestivalStartAt(),
+                festival.getFestivalPosterPath()
+        );
+    }
 }
