06bee5c9a139099cfbc10261671e9cf2e0d76ff3
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-23 04:13:41 +0900

    feat [#144] 타임테이블 시간표 수정 서비스 추가

--- src/main/java/org/sopt/confeti/domain/usertimetable/infra/repository/UserTimetableRepository.java
+++ src/main/java/org/sopt/confeti/domain/usertimetable/infra/repository/UserTimetableRepository.java
@@ -2,6 +2,13 @@ package org.sopt.confeti.domain.usertimetable.infra.repository;
 
 import org.sopt.confeti.domain.usertimetable.UserTimetable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserTimetableRepository extends JpaRepository<UserTimetable, Long> {
+
+    @Query("SELECT ut FROM UserTimetable ut WHERE ut.timetableFestival.user.id = :userId")
+    List<UserTimetable> findByUserId(@Param("userId") long userId);
 }
