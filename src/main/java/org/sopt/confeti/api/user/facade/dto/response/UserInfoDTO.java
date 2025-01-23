611e172373350c4cca2038747cfbb26818811a49
chyun <och2510@gmail.com>
2025-01-23 18:32:36 +0900

    refactor [#167] 컨트롤러 계층에서 url 생성

--- src/main/java/org/sopt/confeti/api/user/facade/dto/response/UserInfoDTO.java
+++ src/main/java/org/sopt/confeti/api/user/facade/dto/response/UserInfoDTO.java
@@ -2,8 +2,16 @@ package org.sopt.confeti.api.user.facade.dto.response;
 
 import org.sopt.confeti.domain.user.User;
 
-public record UserInfoDTO (Long userId, String profileUrl, String username){
-    public static UserInfoDTO from (User user) {
-        return new UserInfoDTO(user.getId(), user.getProfilePath(), user.getUsername());
+public record UserInfoDTO (
+        long userId,
+        String profilePath,
+        String username
+){
+    public static UserInfoDTO from (final User user) {
+        return new UserInfoDTO(
+                user.getId(),
+                user.getProfilePath(),
+                user.getUsername()
+        );
     }
 }
