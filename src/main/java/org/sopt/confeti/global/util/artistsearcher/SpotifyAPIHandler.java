9b932b1b62c55689e2d039664a42503349c4dd14
chyun <och2510@gmail.com>
2025-01-25 03:08:47 +0900

    style [#216] 불필요한 코드 삭제

--- src/main/java/org/sopt/confeti/global/util/artistsearcher/SpotifyAPIHandler.java
+++ src/main/java/org/sopt/confeti/global/util/artistsearcher/SpotifyAPIHandler.java
@@ -130,8 +130,6 @@ public class SpotifyAPIHandler {
     }
 
     public List<ConfetiArtist> findArtistsByArtistIds(final List<String> artistIds) {
-        artistIds.forEach(artistId -> System.out.print(artistId + ","));
-        System.out.println();
         if (artistIds.isEmpty()) {
             return Collections.emptyList();
         }
