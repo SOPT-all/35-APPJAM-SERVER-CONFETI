751be6672e594e2ffffcc617f3462f7e3e0eb7c7
ivoryeee <psa109@sookmyung.ac.kr>
2025-02-25 20:49:42 +0900

    feat [#233] CORS 허용 도메인 추가

--- src/main/java/org/sopt/confeti/global/config/CorsConfig.java
+++ src/main/java/org/sopt/confeti/global/config/CorsConfig.java
@@ -20,6 +20,7 @@ public class CorsConfig {
         allowedOriginPatterns.add("http://localhost:5174");
         allowedOriginPatterns.add("http://172.30.1.58:5173/");
         allowedOriginPatterns.add("http://172.30.1.39:5173/");
+        allowedOriginPatterns.add("http://192.168.3.21:5173/");
         allowedOriginPatterns.add("https://35-appjam-web-confeti.vercel.app/");
         allowedOriginPatterns.add("https://confeti.co.kr");
         allowedOriginPatterns.add("https://www.confeti.co.kr");
