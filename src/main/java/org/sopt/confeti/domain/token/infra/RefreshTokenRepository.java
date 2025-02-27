9d5ef7b24c6ee3db4a177d35287768b4b1161e43
ivoryeee <psa109@sookmyung.ac.kr>
2025-02-27 14:28:36 +0900

    feat [#232] RefreshTokenRepository 생성

---
+++ src/main/java/org/sopt/confeti/domain/token/infra/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package org.sopt.confeti.domain.token.infra;
+
+import org.sopt.confeti.domain.token.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+}
