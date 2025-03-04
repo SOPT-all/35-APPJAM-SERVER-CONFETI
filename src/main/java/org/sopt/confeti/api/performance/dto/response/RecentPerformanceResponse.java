8ef1af43106d3ef317645cefc2b96a8fbf048722
chyun <och2510@gmail.com>
2025-03-04 19:13:02 +0900

    refactor [#228] S3FileHandler 리팩토링에 따른 로직 수정

--- src/main/java/org/sopt/confeti/api/performance/dto/response/RecentPerformanceResponse.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/response/RecentPerformanceResponse.java
@@ -1,6 +1,7 @@
 package org.sopt.confeti.api.performance.dto.response;
 
 import org.sopt.confeti.api.performance.facade.dto.response.RecentPerformanceDTO;
+import org.sopt.confeti.global.common.constant.FolderPath;
 import org.sopt.confeti.global.util.DateConvertor;
 import org.sopt.confeti.global.util.S3FileHandler;
 
@@ -14,6 +15,8 @@ public record RecentPerformanceResponse(
         String posterUrl
 ) {
     public static RecentPerformanceResponse of(final RecentPerformanceDTO recentPerformanceDTO, final S3FileHandler s3FileHandler) {
+        FolderPath topFolder = FolderPath.getFolderPathByPerformanceType(recentPerformanceDTO.type());
+
         return new RecentPerformanceResponse(
                 recentPerformanceDTO.performanceId(),
                 recentPerformanceDTO.typeId(),
@@ -21,7 +24,7 @@ public record RecentPerformanceResponse(
                 recentPerformanceDTO.title(),
                 recentPerformanceDTO.subtitle(),
                 DateConvertor.convertToLocalDate(recentPerformanceDTO.performanceAt()),
-                s3FileHandler.getFileUrl(recentPerformanceDTO.posterPath())
+                s3FileHandler.getFileUrl(FolderPath.combine(topFolder, FolderPath.POSTER), recentPerformanceDTO.posterPath()).getPath()
         );
     }
 }
--- src/main/java/org/sopt/confeti/api/user/dto/response/UserTimetableDetailFestivalResponse.java
+++ src/main/java/org/sopt/confeti/api/user/dto/response/UserTimetableDetailFestivalResponse.java
@@ -1,6 +1,7 @@
 package org.sopt.confeti.api.user.dto.response;
 
 import org.sopt.confeti.api.user.facade.dto.response.UserTimetableFestivalDTO;
+import org.sopt.confeti.global.common.constant.FolderPath;
 import org.sopt.confeti.global.util.S3FileHandler;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public record UserTimetableDetailFestivalResponse(
         return new UserTimetableDetailFestivalResponse(
                 userTimetableFestivalDTO.festivalId(),
                 userTimetableFestivalDTO.title(),
-                s3FileHandler.getFileUrl(userTimetableFestivalDTO.logoPath()),
+                s3FileHandler.getFileUrl(FolderPath.combine(FolderPath.FESTIVAL, FolderPath.LOGO), userTimetableFestivalDTO.logoPath()).getPath(),
                 userTimetableFestivalDTO.festivalDates().stream()
                         .map(UserTimetableDetailDatesResponse::from)
                         .toList()
