297003d6a90ddcdf4528c8336d9ffc8d41ffbb03
chyun <och2510@gmail.com>
2025-03-08 01:27:11 +0900

    feat [#237] JwtTokenExtractor에 Provider 추출 함수 작성

--- src/main/java/org/sopt/confeti/domain/auth/jwt/JwtTokenExtractor.java
+++ src/main/java/org/sopt/confeti/domain/auth/jwt/JwtTokenExtractor.java
@@ -2,6 +2,7 @@ package org.sopt.confeti.domain.auth.jwt;
 
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import org.sopt.confeti.domain.user.OAuthProvider;
 import org.sopt.confeti.domain.user.constant.Role;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 public class JwtTokenExtractor {
 
     private static final String JWT_CLAIM_ROLE = "role";
+    private static final String JWT_CLAIM_PROVIDER = "provider";
 
     private final JwtProperties jwtProperties;
     private final KeyGenerator keyGenerator;
@@ -24,13 +26,20 @@ public class JwtTokenExtractor {
     }
 
     public Role getRole(String token) {
-        String role = Jwts.parser()
+        return Jwts.parser()
                 .verifyWith(keyGenerator.getKeyFromString(jwtProperties.secretKey()))
                 .build()
                 .parseSignedClaims(token)
                 .getPayload()
-                .get(JWT_CLAIM_ROLE, String.class);
+                .get(JWT_CLAIM_ROLE, Role.class);
+    }
 
-        return Role.from(role);
+    public OAuthProvider getProvider(String token) {
+        return Jwts.parser()
+                .verifyWith(keyGenerator.getKeyFromString(jwtProperties.secretKey()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get(JWT_CLAIM_PROVIDER, OAuthProvider.class);
     }
 }
