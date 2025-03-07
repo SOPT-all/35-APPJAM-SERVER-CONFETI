f2103e9ba12d6df92f63eb14a93f022955719b10
chyun <och2510@gmail.com>
2025-03-08 01:24:19 +0900

    feat [#237] 애플 서버 엑세스 토큰 및 리프레시 토큰 관리 테이블 생성, 관리 로직 추가

--- src/main/java/org/sopt/confeti/domain/auth/LogoutService.java
+++ src/main/java/org/sopt/confeti/domain/auth/LogoutService.java
@@ -1,6 +1,9 @@
 package org.sopt.confeti.domain.auth;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.sopt.confeti.domain.token.AppleToken;
+import org.sopt.confeti.domain.token.infra.AppleTokenRepository;
 import org.sopt.confeti.domain.token.infra.RefreshTokenRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,10 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class LogoutService {
+
     private final RefreshTokenRepository refreshTokenRepository;
+    private final AppleTokenRepository appleTokenRepository;
 
     @Transactional
     public void logout(long userId) {
         refreshTokenRepository.deleteAllByUserId(userId);
+        removeSocialTokenIfPresent(userId);
+    }
+
+    @Transactional
+    protected void removeSocialTokenIfPresent(long userId) {
+        Optional<AppleToken> token = appleTokenRepository.findByUserId(userId);
+        token.ifPresent(appleTokenRepository::delete);
     }
 }
---
+++ src/main/java/org/sopt/confeti/domain/auth/dto/apple/AppleTokenInfoResult.java
@@ -0,0 +1,20 @@
+package org.sopt.confeti.domain.auth.dto.apple;
+
+import org.sopt.confeti.global.exception.ConfetiException;
+import org.sopt.confeti.global.message.ErrorMessage;
+
+public record AppleTokenInfoResult(
+        String accessToken,
+        String refreshToken,
+        long expiresIn
+) {
+    public static AppleTokenInfoResult create(String accessToken, String refreshToken, String expiresIn) {
+        try {
+            return new AppleTokenInfoResult(
+                    accessToken, refreshToken, Long.parseLong(expiresIn)
+            );
+        } catch (NumberFormatException e) {
+            throw new ConfetiException(ErrorMessage.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
