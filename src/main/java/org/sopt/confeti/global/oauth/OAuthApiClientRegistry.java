ec7cc3ee93355d6202d2e00064356cb4a272ee62
chyun <och2510@gmail.com>
2025-03-11 02:19:34 +0900

    refactor [#237] @RequiredArgsConstructor의 생성자 생성을 위해 필드 변수에 final 키워드 추가

--- src/main/java/org/sopt/confeti/global/oauth/OAuthApiClientRegistry.java
+++ src/main/java/org/sopt/confeti/global/oauth/OAuthApiClientRegistry.java
@@ -11,7 +11,7 @@ import org.sopt.confeti.global.message.ErrorMessage;
 @RequiredArgsConstructor
 public class OAuthApiClientRegistry {
 
-    private List<OAuthApiClient> oAuthApiClients;
+    private final List<OAuthApiClient> oAuthApiClients;
 
     public OAuthApiClient getOAuthApiClientByProvider(OAuthProvider provider) {
         return oAuthApiClients.stream()
