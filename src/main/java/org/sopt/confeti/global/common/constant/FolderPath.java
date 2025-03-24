74e4e63312fab8d9097adb02d6808b5e2f4bd15a
chyun <och2510@gmail.com>
2025-03-24 17:26:30 +0900

    feat [#244] FolderPath에 Reservation 키워드 추가

--- src/main/java/org/sopt/confeti/global/common/constant/FolderPath.java
+++ src/main/java/org/sopt/confeti/global/common/constant/FolderPath.java
@@ -7,7 +7,7 @@ import org.sopt.confeti.global.message.ErrorMessage;
 public enum FolderPath {
     FESTIVAL("festival"), CONCERT("concert"), USER("user"),
     DETAIL("detail"), LOGO("logo"), MAIN_BANNER("main-banner"),
-    POSTER_BG("poster-bg"), POSTER("poster");
+    POSTER_BG("poster-bg"), POSTER("poster"), RESERVATION("reservation");
 
     private static final String PATH_DELIMITER = "/";
     private final String path;
