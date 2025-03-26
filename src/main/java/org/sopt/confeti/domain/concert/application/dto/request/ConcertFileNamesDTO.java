79bdbaea6fe36a37a78d37d876cb22cf95f60943
ivoryeee <psa109@sookmyung.ac.kr>
2025-03-26 15:51:12 +0900

    refactor [#268] 기타 DTO에서의 reservationBgUrl 필드 삭제

--- src/main/java/org/sopt/confeti/domain/concert/application/dto/request/ConcertFileNamesDTO.java
+++ src/main/java/org/sopt/confeti/domain/concert/application/dto/request/ConcertFileNamesDTO.java
@@ -3,12 +3,11 @@ package org.sopt.confeti.domain.concert.application.dto.request;
 public record ConcertFileNamesDTO(
         String poster,
         String posterBg,
-        String infoImg,
-        String reservationBg
+        String infoImg
 ) {
     public static ConcertFileNamesDTO of(
-            final String poster, final String posterBg, final String infoImg, final String reservationBg
+            final String poster, final String posterBg, final String infoImg
     ) {
-        return new ConcertFileNamesDTO(poster, posterBg, infoImg, reservationBg);
+        return new ConcertFileNamesDTO(poster, posterBg, infoImg);
     }
 }
