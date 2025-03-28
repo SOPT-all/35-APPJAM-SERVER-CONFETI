d4544626a169b33514c368ae0ce345cf7b7eaec6
chyun <och2510@gmail.com>
2025-03-28 17:55:20 +0900

    refactor [#260] 아티스트, 앨범, 음악 객체 그래프 전략 패턴 통합

--- src/main/java/org/sopt/confeti/global/resolver/music_api/artist/strategy/ArtistStrategy.java
+++ src/main/java/org/sopt/confeti/global/resolver/music_api/artist/strategy/ArtistStrategy.java
@@ -1,15 +1,10 @@
 package org.sopt.confeti.global.resolver.music_api.artist.strategy;
 
-import java.util.HashMap;
-import java.util.Queue;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.sopt.confeti.global.resolver.music_api.artist.vo.ConfetiArtist;
+import org.sopt.confeti.global.resolver.music_api.strategy.AbstractMusicAPIStrategy;
 
-public interface ArtistStrategy {
-
-    void collect(
-            HashMap<String, Queue<ConfetiArtist>> artistMapper,
-            Object target
-    );
-
-    boolean supports(Class<?> clazz);
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class ArtistStrategy extends AbstractMusicAPIStrategy<ConfetiArtist> {
 }
