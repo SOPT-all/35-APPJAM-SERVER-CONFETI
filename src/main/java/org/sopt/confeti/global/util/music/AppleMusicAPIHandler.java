5eeee5346fd4dffe9b3f919686c23d75b43318ca
chyun <och2510@gmail.com>
2025-03-28 17:06:25 +0900

    style [#260] ArtistResolver -> MusicAPIResolver로 네이밍 수정

--- src/main/java/org/sopt/confeti/global/util/music/AppleMusicAPIHandler.java
+++ src/main/java/org/sopt/confeti/global/util/music/AppleMusicAPIHandler.java
@@ -1,7 +1,6 @@
 package org.sopt.confeti.global.util.music;
 
 import jakarta.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -20,7 +19,7 @@ import org.sopt.confeti.global.resolver.music_api.artist.vo.ConfetiArtist;
 import org.sopt.confeti.global.resolver.music_api.music.vo.ConfetiMusic;
 import org.sopt.confeti.global.util.music.dto.album.AppleMusicAlbumsResponse;
 import org.sopt.confeti.global.module.web_client.builder.ApiWebClientBuilder;
-import org.sopt.confeti.global.resolver.artist.ConfetiArtist;
+import org.sopt.confeti.global.resolver.artist.vo.ConfetiArtist;
 import org.sopt.confeti.global.util.music.dto.artist.AppleMusicArtistsResponse;
 import org.sopt.confeti.global.util.music.dto.music.AppleMusicMusicsResponse;
 import org.sopt.confeti.global.util.music.dto.search.AppleMusicSearchResponse;
