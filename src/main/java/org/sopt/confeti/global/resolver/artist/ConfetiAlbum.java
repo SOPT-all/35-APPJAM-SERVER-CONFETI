4a9b27c6d4f8ce2f3ed1edf6774f6a16a612110e
chyun <och2510@gmail.com>
2025-03-28 14:40:08 +0900

    feat [#260] 아티스트, 앨범 클래스 작성

---
+++ src/main/java/org/sopt/confeti/global/resolver/artist/ConfetiAlbum.java
@@ -0,0 +1,27 @@
+package org.sopt.confeti.global.resolver.artist;
+
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.sopt.confeti.global.util.music.dto.artist.AppleMusicArtistAlbumResponse;
+
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class ConfetiAlbum {
+
+    private String id;
+
+    @Setter
+    private String name;
+
+    @Setter
+    private LocalDate releaseAt;
+
+    public static ConfetiAlbum from(final AppleMusicArtistAlbumResponse albumResponse) {
+        return new ConfetiAlbum(
+                albumResponse.id(),
+                "",
+                null
+        );
+    }
+}
