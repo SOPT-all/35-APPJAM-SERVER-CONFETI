2f836511132818b0822a668a3f75feca3c484f74
chyun <och2510@gmail.com>
2025-03-28 23:16:26 +0900

    refactor [#260] RestClient로 교체

--- src/main/java/org/sopt/confeti/global/util/music/MusicAPIHandler.java
+++ src/main/java/org/sopt/confeti/global/util/music/MusicAPIHandler.java
@@ -3,12 +3,14 @@ package org.sopt.confeti.global.util.music;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import org.sopt.confeti.global.resolver.music_api.album.vo.ConfetiAlbum;
 import org.sopt.confeti.global.resolver.music_api.artist.vo.ConfetiArtist;
-import reactor.core.publisher.Mono;
 
 public interface MusicAPIHandler {
 
-    Mono<List<ConfetiArtist>> getArtistsByArtistIds(final Set<String> artistIds);
-    Mono<Optional<ConfetiArtist>> findArtistByKeyword(final String keyword);
-    Mono<Optional<ConfetiArtist>> findArtistByArtistId(final String artistId);
+    List<ConfetiArtist> getArtistsByArtistIds(final Set<String> artistIds);
+    Optional<ConfetiArtist> findArtistByKeyword(final String keyword);
+    Optional<ConfetiArtist> findArtistByArtistId(final String artistId);
+
+    List<ConfetiAlbum> getAlbumsByAlbumIds(final Set<String> albumIds);
 }
