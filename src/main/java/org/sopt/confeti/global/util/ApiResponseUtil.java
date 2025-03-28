cd5ba35e1c40481e11d44fd1c9bcae89d0337745
chyun <och2510@gmail.com>
2025-03-28 14:40:53 +0900

    feat [#260] ApiResponseUtil에 Mono 응답 추가

--- src/main/java/org/sopt/confeti/global/util/ApiResponseUtil.java
+++ src/main/java/org/sopt/confeti/global/util/ApiResponseUtil.java
@@ -4,6 +4,7 @@ import org.sopt.confeti.global.common.BaseResponse;
 import org.sopt.confeti.global.message.ErrorMessage;
 import org.sopt.confeti.global.message.SuccessMessage;
 import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Mono;
 
 public interface ApiResponseUtil {
     static ResponseEntity<BaseResponse<?>> success(SuccessMessage successMessage) {
@@ -14,6 +15,13 @@ public interface ApiResponseUtil {
     static <T> ResponseEntity<BaseResponse<?>> success(SuccessMessage successMessage, T data) {
         return ResponseEntity.status(successMessage.getHttpStatus())
                 .body(BaseResponse.of(successMessage, data));
+    }
+
+    static <T> Mono<ResponseEntity<BaseResponse<?>>> success(SuccessMessage successMessage, Mono<T> data) {
+        return data.map(result ->
+                ResponseEntity.status(successMessage.getHttpStatus())
+                .body(BaseResponse.of(successMessage, result))
+        );
     }
 
     static ResponseEntity<BaseResponse<?>> failure(ErrorMessage errorMessage) {
