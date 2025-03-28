95b12ce94f14a7e87cafde9e3274eab1a21fffa0
chyun <och2510@gmail.com>
2025-03-28 23:16:26 +0900

    refactor [#260] RestClient로 교체

--- src/main/java/org/sopt/confeti/global/util/music/AppleMusicAPIHandler.java
+++ src/main/java/org/sopt/confeti/global/util/music/AppleMusicAPIHandler.java
@@ -19,12 +19,14 @@ import org.sopt.confeti.global.resolver.music_api.artist.vo.ConfetiArtist;
 import org.sopt.confeti.global.resolver.music_api.music.vo.ConfetiMusic;
 import org.sopt.confeti.global.util.music.dto.album.AppleMusicAlbumsResponse;
 import org.sopt.confeti.global.module.web_client.builder.ApiWebClientBuilder;
+import org.sopt.confeti.global.resolver.music_api.album.vo.ConfetiAlbum;
 import org.sopt.confeti.global.resolver.music_api.artist.vo.ConfetiArtist;
 import org.sopt.confeti.global.util.music.dto.artist.AppleMusicArtistsResponse;
 import org.sopt.confeti.global.util.music.dto.music.AppleMusicMusicsResponse;
 import org.sopt.confeti.global.util.music.dto.search.AppleMusicSearchResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
