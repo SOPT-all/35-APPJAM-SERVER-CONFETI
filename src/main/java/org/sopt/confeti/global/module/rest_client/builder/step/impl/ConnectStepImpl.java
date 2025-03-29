9bcba7b72b7c4e42938994e7bb5154015ff9ebb6
chyun <och2510@gmail.com>
2025-03-30 00:34:13 +0900

    feat [#260] 헤더 없는 connect 함수 추가

--- src/main/java/org/sopt/confeti/global/module/rest_client/builder/step/impl/ConnectStepImpl.java
+++ src/main/java/org/sopt/confeti/global/module/rest_client/builder/step/impl/ConnectStepImpl.java
@@ -17,6 +17,23 @@ public class ConnectStepImpl implements ConnectStep {
     private final RestClient.RequestHeadersSpec<?> methodType;
 
     /**
+     * RestClient header 설정 없이 요청 전송
+     * @return 다음 ResponseStep 반환
+     */
+    @Override
+    public ResponseStep connect() {
+        try {
+            return new ResponseStepImpl(
+                    this.methodType
+                            .retrieve()
+            );
+        } catch (Exception e) {
+            log.error("Exception: {} | {}", e.getMessage(), e.getStackTrace()[0].toString());
+            throw new ConfetiException(ErrorMessage.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
      * RestClient header 설정 후 요청 전송
      * @param headers
      * @return 다음 ResponseStep 반환
