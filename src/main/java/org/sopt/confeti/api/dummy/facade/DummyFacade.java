6ba5976c0a800180d4b875081e1047863461a7ef
chyun <och2510@gmail.com>
2025-04-02 16:28:16 +0900

    feat [#276] 등록 요청된 페스티벌을 Performance에 추가

--- src/main/java/org/sopt/confeti/api/dummy/facade/DummyFacade.java
+++ src/main/java/org/sopt/confeti/api/dummy/facade/DummyFacade.java
@@ -6,12 +6,15 @@ import org.sopt.confeti.api.dummy.facade.dto.concert.ConcertFilePathsDTO;
 import org.sopt.confeti.api.dummy.facade.dto.concert.request.CreateConcertDTO;
 import org.sopt.confeti.api.dummy.facade.dto.concert.request.UploadConcertFilesDTO;
 import org.sopt.confeti.api.dummy.facade.dto.festival.FestivalFilePathsDTO;
+import org.sopt.confeti.api.dummy.facade.dto.festival.request.CreateFestivalArtistDTO;
 import org.sopt.confeti.api.dummy.facade.dto.festival.request.CreateFestivalDTO;
 import org.sopt.confeti.api.dummy.facade.dto.festival.request.UploadFestivalFilesDTO;
 import org.sopt.confeti.domain.concert.Concert;
 import org.sopt.confeti.domain.concert.application.ConcertService;
 import org.sopt.confeti.domain.festival.Festival;
 import org.sopt.confeti.domain.festival.application.FestivalService;
+import org.sopt.confeti.domain.view.performance.Performance;
+import org.sopt.confeti.domain.view.performance.application.PerformanceService;
 import org.sopt.confeti.global.annotation.Facade;
 import org.sopt.confeti.global.common.constant.FolderPath;
 import org.sopt.confeti.global.util.S3FileHandler;
@@ -24,6 +27,7 @@ public class DummyFacade {
     private final S3FileHandler s3FileHandler;
     private final FestivalService festivalService;
     private final ConcertService concertService;
+    private final PerformanceService performanceService;
 
     public FestivalFilePathsDTO uploadFestivalFiles(UploadFestivalFilesDTO files) {
         String posterPath = s3FileHandler.uploadFile(files.poster(),
@@ -42,7 +46,23 @@ public class DummyFacade {
 
     @Transactional
     public void createFestival(CreateFestivalDTO festivalDTO) {
-        festivalService.create(Festival.create(festivalDTO));
+        long festivalId = festivalService.create(Festival.create(festivalDTO));
+        performanceService.create(createPerformances(festivalId, festivalDTO));
+    }
+
+    private List<Performance> createPerformances(long festivalId, CreateFestivalDTO festivalDTO) {
+        return getArtistIds(festivalDTO).stream()
+                .map(artistId -> Performance.create(festivalId, festivalDTO, artistId))
+                .toList();
+    }
+
+    private List<String> getArtistIds(CreateFestivalDTO festivalDTO) {
+        return festivalDTO.dates().stream()
+                .flatMap(date -> date.stages().stream())
+                .flatMap(stage -> stage.times().stream())
+                .flatMap(time -> time.artists().stream())
+                .map(CreateFestivalArtistDTO::artistId)
+                .toList();
     }
 
     public ConcertFilePathsDTO uploadConcertFiles(UploadConcertFilesDTO files) {
