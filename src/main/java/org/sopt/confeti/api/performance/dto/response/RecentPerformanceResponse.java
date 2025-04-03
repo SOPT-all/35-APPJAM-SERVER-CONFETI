2e989889866a5a8e2b62ee955266714a8f7a5b93
chyun <och2510@gmail.com>
2025-04-04 01:24:27 +0900

    refactor [#280] 응답에서 공연 부제목 제거, 공연 시작 날짜 변수명 수정 (performanceAt -> startAt)

--- src/main/java/org/sopt/confeti/api/performance/dto/response/RecentPerformanceResponse.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/response/RecentPerformanceResponse.java
@@ -10,8 +10,7 @@ public record RecentPerformanceResponse(
         long typeId,
         String type,
         String title,
-        String subtitle,
-        String performanceAt,
+        String startAt,
         String posterUrl
 ) {
     public static RecentPerformanceResponse of(final RecentPerformanceDTO recentPerformanceDTO,
@@ -23,7 +22,6 @@ public record RecentPerformanceResponse(
                 recentPerformanceDTO.typeId(),
                 recentPerformanceDTO.type().getType(),
                 recentPerformanceDTO.title(),
-                recentPerformanceDTO.subtitle(),
                 DateConvertor.convertToDefaultFormat(recentPerformanceDTO.performanceAt()),
                 s3FileHandler.getFileUrl(FolderPath.combine(topFolder, FolderPath.POSTER),
                         recentPerformanceDTO.posterPath()).toString()
