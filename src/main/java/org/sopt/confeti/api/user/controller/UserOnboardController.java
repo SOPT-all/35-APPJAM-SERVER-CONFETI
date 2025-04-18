56c605e32940060f899be5761df22a3a92f1adf3
chyun <och2510@gmail.com>
2025-04-18 11:41:14 +0900

    fix [#306] 엑세스 토큰 인증 처리

--- src/main/java/org/sopt/confeti/api/user/controller/UserOnboardController.java
+++ src/main/java/org/sopt/confeti/api/user/controller/UserOnboardController.java
@@ -12,14 +12,13 @@ import org.sopt.confeti.domain.user.constant.Role;
 import org.sopt.confeti.global.annotation.Permission;
 import org.sopt.confeti.global.annotation.UserId;
 import org.sopt.confeti.global.annotation.UserId;
+import org.sopt.confeti.global.annotation.UserId;
 import org.sopt.confeti.global.common.BaseResponse;
 import org.sopt.confeti.global.message.SuccessMessage;
 import org.sopt.confeti.global.util.ApiResponseUtil;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +34,7 @@ public class UserOnboardController {
     @Permission(role = {Role.ONBOARDING})
     @GetMapping("/artists/search")
     public ResponseEntity<BaseResponse<?>> getArtistsRelatedTerm(
-            @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken,
+            @UserId Long userId,
             @RequestParam String term,
             @RequestParam(defaultValue = "1") @Min(1) @Max(25) Integer limit
     ) {
