5307506674e13f5e268c7543b688f41e55bdfb13
ivoryeee <psa109@sookmyung.ac.kr>
2025-04-20 17:46:43 +0900

    feat [#312] ArtistFavorite 아티스트 좋아요 추가 일시 정보(createdAt) 추가

--- src/main/java/org/sopt/confeti/domain/artist_favorite/ArtistFavorite.java
+++ src/main/java/org/sopt/confeti/domain/artist_favorite/ArtistFavorite.java
@@ -1,20 +1,15 @@
 package org.sopt.confeti.domain.artist_favorite;
 
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt.confeti.domain.user.User;
 import org.sopt.confeti.global.resolver.music_api.artist.vo.ConfetiArtist;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "artist_favorites")
@@ -30,19 +25,24 @@ public class ArtistFavorite {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
     @Embedded
     private ConfetiArtist artist;
 
     @Builder
-    private ArtistFavorite(User user, String artistId) {
+    private ArtistFavorite(User user, String artistId, LocalDateTime createdAt) {
         this.user = user;
         this.artist = ConfetiArtist.from(artistId);
+        this.createdAt = LocalDateTime.now();
     }
 
     public static ArtistFavorite create(User user, String artistId) {
         return ArtistFavorite.builder()
                 .user(user)
                 .artistId(artistId)
+                .createdAt(LocalDateTime.now())
                 .build();
     }
 }
