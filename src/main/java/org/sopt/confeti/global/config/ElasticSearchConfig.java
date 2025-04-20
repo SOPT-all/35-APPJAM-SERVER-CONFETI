6cd5d501d4a94df9e0a31eb6fc266136ceb3b443
chyun <och2510@gmail.com>
2025-04-20 22:34:12 +0900

    refactor [#310] Spring Data ElasticSearch 구성 정보 추가

--- src/main/java/org/sopt/confeti/global/config/ElasticSearchConfig.java
+++ src/main/java/org/sopt/confeti/global/config/ElasticSearchConfig.java
@@ -12,13 +12,21 @@ import org.springframework.data.elasticsearch.repository.config.EnableReactiveEl
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
     @Value("${spring.data.elasticsearch.uris}")
-    private String[] esHosts;
+    private String esHost;
+
+    @Value("${spring.data.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.data.elasticsearch.password}")
+    private String password;
 
     @NonNull
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo(esHosts)
+                .connectedTo(esHost)
+                .usingSsl()
+                .withBasicAuth(username, password)
                 .build();
     }
 }
