c6c3d8bce07b1b39abae57b9595c62d72e109f42
ivoryeee <psa109@sookmyung.ac.kr>
2025-04-24 00:58:06 +0900

    refactor [#347] 추천 음악 조회 시 musicId도 반환하도록 응답값 추가

--- src/main/java/org/sopt/confeti/api/performance/dto/response/RecommendMusicResponse.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/response/RecommendMusicResponse.java
@@ -3,6 +3,7 @@ package org.sopt.confeti.api.performance.dto.response;
 import org.sopt.confeti.api.performance.facade.dto.response.RecommendMusicDTO;
 
 public record RecommendMusicResponse(
+        String musicId,
         String artistName,
         String title,
         String artWorkUrl,
@@ -10,6 +11,7 @@ public record RecommendMusicResponse(
 ) {
     public static RecommendMusicResponse from(RecommendMusicDTO recommendMusicDTO) {
         return new RecommendMusicResponse(
+                recommendMusicDTO.id(),
                 recommendMusicDTO.artistName(),
                 recommendMusicDTO.title(),
                 recommendMusicDTO.artWorkUrl(),
