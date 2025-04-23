9f3775bcaddb6dbdeec4ecafe638b03c8456877b
Willy <willy_1020@naver.com>
2025-04-23 14:56:55 +0900

    feat [#274] 셋리스트의 곡을 담을 엔티티 추가

---
+++ src/main/java/org/sopt/confeti/domain/setlist/SetlistMusic.java
@@ -0,0 +1,52 @@
+package org.sopt.confeti.domain.setlist;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "setlist_musics")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class SetlistMusic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "setlist_id", nullable = false)
+    private Setlist setlist;
+
+    @Column(name = "artist_name", nullable = false)
+    private String artistName;
+
+    @Column(name = "track_name", nullable = false)
+    private String trackName;
+
+    @Column(name = "artwork_url")
+    private String artworkUrl;
+
+    @Column(name = "preview_url")
+    private String previewUrl;
+
+    @Column(name = "orders", nullable = false)
+    private int orders;
+
+    @Builder
+    public SetlistMusic(String artistName, String trackName, String artworkUrl, String previewUrl, int orders) {
+        this.artistName = artistName;
+        this.trackName = trackName;
+        this.artworkUrl = artworkUrl;
+        this.previewUrl = previewUrl;
+        this.orders = orders;
+    }
+
+    public void setSetlist(Setlist setlist) {
+        this.setlist = setlist;
+    }
+
+    public void changeOrder(int newOrder) {
+        this.orders = newOrder;
+    }
+}
