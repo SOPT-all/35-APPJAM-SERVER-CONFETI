dc92b922201148c3c41e54323450bd1e23f9669d
Willy <willy_1020@naver.com>
2025-04-24 03:54:39 +0900

    feat [#363] 특정 셋리스트 곡 순서 변경 API 구현 (Controller)

--- src/main/java/org/sopt/confeti/api/setlist/SetlistEditController.java
+++ src/main/java/org/sopt/confeti/api/setlist/SetlistEditController.java
@@ -1,14 +1,19 @@
 package org.sopt.confeti.api.setlist;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.sopt.confeti.domain.setlist.application.SetlistEditService;
+import org.sopt.confeti.domain.setlist.application.dto.request.SetlistMusicOrderUpdateRequest;
 import org.sopt.confeti.global.annotation.UserId;
 import org.sopt.confeti.global.common.BaseResponse;
 import org.sopt.confeti.global.message.SuccessMessage;
 import org.sopt.confeti.global.util.ApiResponseUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +31,15 @@ public class SetlistEditController {
     ) {
         setlistEditService.startEdit(userId, setlistId);
         return ApiResponseUtil.success(SuccessMessage.CREATED);
+    }
+
+    @PatchMapping("/{setlistId}/edit/musics/order")
+    public ResponseEntity<BaseResponse<?>> updateMusicOrder(
+            @UserId(require = false) Long userId,
+            @PathVariable Long setlistId,
+            @RequestBody List<SetlistMusicOrderUpdateRequest> request
+    ) {
+        setlistEditService.updateMusicOrder(userId, setlistId, request);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS);
     }
 }
