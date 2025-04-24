a61006d674c20ea8c6416b9f1c3330bdbdd8b5de
ivoryeee <psa109@sookmyung.ac.kr>
2025-04-24 21:16:14 +0900

    refactor [#379] 추천 음악 대상 공연 조회 시의 DTO, response 응답값 수정

--- src/main/java/org/sopt/confeti/api/performance/dto/response/RecommendMusicsResponse.java
+++ src/main/java/org/sopt/confeti/api/performance/dto/response/RecommendMusicsResponse.java
@@ -1,24 +1,14 @@
 package org.sopt.confeti.api.performance.dto.response;
 
 import org.sopt.confeti.api.performance.facade.dto.response.RecommendMusicsDTO;
-import org.sopt.confeti.global.common.constant.PerformanceType;
-
 import java.util.List;
 
 public record RecommendMusicsResponse(
-        Long id,
-        Long typeId,
-        PerformanceType type,
-        String title,
         List<RecommendMusicResponse> musicList
 ) {
-    public static RecommendMusicsResponse from(RecommendMusicsDTO recommendMusicsDTO) {
+    public static RecommendMusicsResponse from(RecommendMusicsDTO musicListDTO) {
         return new RecommendMusicsResponse(
-                recommendMusicsDTO.id(),
-                recommendMusicsDTO.typeId(),
-                recommendMusicsDTO.type(),
-                recommendMusicsDTO.title(),
-                recommendMusicsDTO.musicList().stream()
+                musicListDTO.musicList().stream()
                         .map(RecommendMusicResponse::from)
                         .toList()
         );
--- src/main/java/org/sopt/confeti/api/performance/facade/dto/response/RecommendMusicsDTO.java
+++ src/main/java/org/sopt/confeti/api/performance/facade/dto/response/RecommendMusicsDTO.java
@@ -1,24 +1,13 @@
 package org.sopt.confeti.api.performance.facade.dto.response;
 
-import org.sopt.confeti.domain.view.performance.Performance;
-import org.sopt.confeti.global.common.constant.PerformanceType;
 import org.sopt.confeti.global.resolver.music_api.music.vo.ConfetiMusic;
-
 import java.util.List;
 
 public record RecommendMusicsDTO(
-        Long id,
-        Long typeId,
-        PerformanceType type,
-        String title,
         List<RecommendMusicDTO> musicList
 ) {
-    public static RecommendMusicsDTO of(Performance performance, List<ConfetiMusic> musicList) {
+    public static RecommendMusicsDTO from(List<ConfetiMusic> musicList) {
         return new RecommendMusicsDTO(
-                performance.getId(),
-                performance.getTypeId(),
-                performance.getType(),
-                performance.getTitle(),
                 musicList.stream()
                         .map(RecommendMusicDTO::from)
                         .toList()
