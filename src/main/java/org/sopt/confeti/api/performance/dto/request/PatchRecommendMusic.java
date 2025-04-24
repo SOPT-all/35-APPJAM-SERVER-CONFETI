155c928b2229c283dd39a5c46cea4cb73e1ae892
ivoryeee <psa109@sookmyung.ac.kr>
2025-04-24 20:56:28 +0900

    remove [#379] Patch -> GET 변경으로 클라이언트측 요청값 삭제

--- src/main/java/org/sopt/confeti/api/performance/dto/request/PatchRecommendMusic.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.confeti.api.performance.dto.request;
-
-public record PatchRecommendMusic(
-        String musicId
-) {
-    public static PatchRecommendMusic from(PatchRecommendMusic music) {
-        return new PatchRecommendMusic(music.musicId());
-    }
-}
