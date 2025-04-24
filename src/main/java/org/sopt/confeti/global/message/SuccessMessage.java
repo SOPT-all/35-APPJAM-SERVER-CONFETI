899677dcc03afd2e1d70e0a503dd1ae67aa93dc6
Willy <willy_1020@naver.com>
2025-04-24 16:08:01 +0900

    chore [#369] 데이터 삭제 시 공통 응답 메세지 추가

--- src/main/java/org/sopt/confeti/global/message/SuccessMessage.java
+++ src/main/java/org/sopt/confeti/global/message/SuccessMessage.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum SuccessMessage {
     SUCCESS(HttpStatus.OK, "요청이 성공했습니다."),
     CREATED(HttpStatus.CREATED, "생성되었습니다."),
+    DELETED(HttpStatus.OK, "삭제가 완료되었습니다.")
     ;
 
     private final HttpStatus httpStatus;
