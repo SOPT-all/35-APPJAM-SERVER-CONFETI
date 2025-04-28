ea749f3b7d088a6b2a807bf3bcfdbf420873be87
Willy <willy_1020@naver.com>
2025-04-29 01:57:58 +0900

    fix [#403] 셋리스트 상세 조회에서 S3FileHandler.getFileUrl() 을 이용해 S3 Presigned URL 형식으로 받아오도록 수정

--- src/main/java/org/sopt/confeti/domain/setlist/application/dto/response/GetSetlistDetailResponse.java
+++ src/main/java/org/sopt/confeti/domain/setlist/application/dto/response/GetSetlistDetailResponse.java
@@ -2,6 +2,10 @@ package org.sopt.confeti.domain.setlist.application.dto.response;
 
 import java.time.LocalDate;
 import java.util.List;
+import org.sopt.confeti.domain.setlist.Setlist;
+import org.sopt.confeti.domain.setlist.SetlistType;
+import org.sopt.confeti.global.common.constant.FolderPath;
+import org.sopt.confeti.global.util.S3FileHandler;
 
 public record GetSetlistDetailResponse(
         Long setlistId,
@@ -15,4 +19,41 @@ public record GetSetlistDetailResponse(
         LocalDate endAt,
         List<SetlistMusicResponseDto> musics
 ) {
+    public static GetSetlistDetailResponse of(
+            Setlist setlist,
+            String title,
+            String subtitle,
+            String posterPath,
+            String posterBgPath,
+            LocalDate startAt,
+            LocalDate endAt,
+            List<SetlistMusicResponseDto> musics,
+            SetlistType type,
+            S3FileHandler s3FileHandler
+    ) {
+        String posterUrl = s3FileHandler.getFileUrl(
+                FolderPath.combine(
+                        type == SetlistType.CONCERT ? FolderPath.CONCERT : FolderPath.FESTIVAL,
+                        FolderPath.POSTER
+                ), posterPath).toString();
+
+        String posterBgUrl = s3FileHandler.getFileUrl(
+                FolderPath.combine(
+                        type == SetlistType.CONCERT ? FolderPath.CONCERT : FolderPath.FESTIVAL,
+                        FolderPath.POSTER_BG
+                ), posterBgPath).toString();
+
+        return new GetSetlistDetailResponse(
+                setlist.getId(),
+                type.name(),
+                setlist.getTypeId(),
+                posterUrl,
+                posterBgUrl,
+                title,
+                subtitle,
+                startAt,
+                endAt,
+                musics
+        );
+    }
 }
