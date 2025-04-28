feeb987f547412252e8beef40aa88968a2b95fce
Willy <willy_1020@naver.com>
2025-04-29 01:56:39 +0900

    fix [#403] 셋리스트 조회에서 S3FileHandler.getFileUrl() 을 이용해 S3 Presigned URL을 반환하도록 수정

--- src/main/java/org/sopt/confeti/domain/setlist/application/dto/response/SetlistSummaryDto.java
+++ src/main/java/org/sopt/confeti/domain/setlist/application/dto/response/SetlistSummaryDto.java
@@ -2,6 +2,9 @@ package org.sopt.confeti.domain.setlist.application.dto.response;
 
 import java.time.LocalDate;
 import org.sopt.confeti.domain.setlist.Setlist;
+import org.sopt.confeti.domain.setlist.SetlistType;
+import org.sopt.confeti.global.common.constant.FolderPath;
+import org.sopt.confeti.global.util.S3FileHandler;
 
 public record SetlistSummaryDto(
         Long setlistId,
@@ -11,7 +14,19 @@ public record SetlistSummaryDto(
         String posterUrl,
         LocalDate endAt
 ) {
-    public static SetlistSummaryDto of(Setlist setlist, String title, String posterUrl, LocalDate endAt) {
+    public static SetlistSummaryDto of(
+            Setlist setlist,
+            String title,
+            String posterPath,
+            LocalDate endAt,
+            S3FileHandler s3FileHandler
+            ) {
+        String posterUrl =  s3FileHandler.getFileUrl(
+                FolderPath.combine(
+                        setlist.getType() == SetlistType.CONCERT ? FolderPath.CONCERT : FolderPath.FESTIVAL, FolderPath.POSTER
+                ),
+                posterPath
+        ).toString();
         return new SetlistSummaryDto(
                 setlist.getId(),
                 setlist.getType().name(),
