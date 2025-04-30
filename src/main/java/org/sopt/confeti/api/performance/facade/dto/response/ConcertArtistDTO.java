d3093a7ea213644ed20d5d6dfb4b148850b522ff
chyun <och2510@gmail.com>
2025-04-30 16:34:04 +0900

    refactor [#421] 사용되지 않는 앨범 발매일자 제거

--- src/main/java/org/sopt/confeti/api/performance/facade/dto/response/ConcertArtistDTO.java
+++ src/main/java/org/sopt/confeti/api/performance/facade/dto/response/ConcertArtistDTO.java
@@ -1,14 +1,12 @@
 package org.sopt.confeti.api.performance.facade.dto.response;
 
-import java.time.LocalDate;
 import org.sopt.confeti.domain.concert_artist.ConcertArtist;
 import org.sopt.confeti.global.resolver.music_api.artist.vo.ConfetiArtist;
 
 public record ConcertArtistDTO(
         String artistId,
         String name,
-        String profileUrl,
-        LocalDate latestReleaseAt
+        String profileUrl
 ) {
     public static ConcertArtistDTO of(final ConcertArtist concertArtist) {
         ConfetiArtist confetiArtist = concertArtist.getArtist();
@@ -16,8 +14,7 @@ public record ConcertArtistDTO(
         return new ConcertArtistDTO(
                 confetiArtist.getId(),
                 confetiArtist.getName(),
-                confetiArtist.getProfileUrl(),
-                confetiArtist.getLatestReleaseAlbum().getReleaseAt()
+                confetiArtist.getProfileUrl()
         );
     }
 }
--- src/main/java/org/sopt/confeti/api/performance/facade/dto/response/FestivalDetailArtistDTO.java
+++ src/main/java/org/sopt/confeti/api/performance/facade/dto/response/FestivalDetailArtistDTO.java
@@ -1,22 +1,19 @@
 package org.sopt.confeti.api.performance.facade.dto.response;
 
-import java.time.LocalDate;
 import org.sopt.confeti.domain.festival_artist.FestivalArtist;
 import org.sopt.confeti.global.resolver.music_api.artist.vo.ConfetiArtist;
 
 public record FestivalDetailArtistDTO(
         String artistId,
         String name,
-        String profileUrl,
-        LocalDate latestReleaseAt
+        String profileUrl
 ) {
     public static FestivalDetailArtistDTO from(final FestivalArtist festivalArtist) {
         ConfetiArtist confetiArtist = festivalArtist.getArtist();
         return new FestivalDetailArtistDTO(
                 confetiArtist.getId(),
                 confetiArtist.getName(),
-                confetiArtist.getProfileUrl(),
-                confetiArtist.getLatestReleaseAlbum().getReleaseAt()
+                confetiArtist.getProfileUrl()
         );
     }
 }
