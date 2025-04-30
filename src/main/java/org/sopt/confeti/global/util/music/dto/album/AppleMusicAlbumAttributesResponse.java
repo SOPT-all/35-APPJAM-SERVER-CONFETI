0820bdec434c7d43b0713b9216729da6cf77d52f
chyun <och2510@gmail.com>
2025-04-30 16:34:04 +0900

    refactor [#421] 사용되지 않는 앨범 발매일자 제거

--- src/main/java/org/sopt/confeti/global/util/music/dto/album/AppleMusicAlbumAttributesResponse.java
+++ src/main/java/org/sopt/confeti/global/util/music/dto/album/AppleMusicAlbumAttributesResponse.java
@@ -1,9 +1,6 @@
 package org.sopt.confeti.global.util.music.dto.album;
 
-import java.time.LocalDate;
-
 public record AppleMusicAlbumAttributesResponse(
-        LocalDate releaseDate,
         String name
 ) {
 }
