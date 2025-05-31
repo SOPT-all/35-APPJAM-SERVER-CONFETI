f788556334c4d79e7252c369d2c24c8c09948c80
chyun <och2510@gmail.com>
2025-05-31 15:25:04 +0900

    refactor [#446] 더 이상 사용되지 않는 검색 API 제거

--- src/main/java/org/sopt/confeti/api/artist/controller/ArtistController.java
+++ src/main/java/org/sopt/confeti/api/artist/controller/ArtistController.java
@@ -1,18 +1,11 @@
 package org.sopt.confeti.api.artist.controller;
 
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.sopt.confeti.api.artist.dto.response.SearchACArtistsResponse;
-import org.sopt.confeti.api.artist.dto.response.SearchArtistResponse;
 import org.sopt.confeti.api.artist.facade.ArtistFacade;
 import org.sopt.confeti.api.artist.facade.dto.response.SearchACArtistsDTO;
-import org.sopt.confeti.api.artist.facade.dto.response.SearchArtistDTO;
-import org.sopt.confeti.domain.user.constant.Role;
-import org.sopt.confeti.global.annotation.Permission;
 import org.sopt.confeti.global.annotation.UserId;
 import org.sopt.confeti.global.common.BaseResponse;
-import org.sopt.confeti.global.exception.ConfetiException;
-import org.sopt.confeti.global.message.ErrorMessage;
 import org.sopt.confeti.global.message.SuccessMessage;
 import org.sopt.confeti.global.util.ApiResponseUtil;
 import org.springframework.http.ResponseEntity;
@@ -27,21 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ArtistController {
 
     private final ArtistFacade artistFacade;
-
-    @Permission(role = {Role.GENERAL})
-    @GetMapping("/search")
-    public ResponseEntity<BaseResponse<?>> search(
-            @UserId(require = false) Long userId,
-            @RequestParam(required = false) String term,
-            @RequestParam(required = false) String aid
-    ) {
-        if (Objects.isNull(term) && Objects.isNull(aid)) {
-            throw new ConfetiException(ErrorMessage.BAD_REQUEST);
-        }
-
-        SearchArtistDTO artist = artistFacade.search(userId, term, aid);
-        return ApiResponseUtil.success(SuccessMessage.SUCCESS, SearchArtistResponse.from(artist));
-    }
 
     @GetMapping("/search/ac")
     public ResponseEntity<BaseResponse<?>> searchAutoComplete(
