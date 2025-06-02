7ff37070faac2ecd034423bf6ce20241798c7f8d
ivoryeee <psa109@sookmyung.ac.kr>
2025-06-03 00:17:38 +0900

    chore [#454] Swagger 설정을 위한 addResourceHandlers 추가

--- src/main/java/org/sopt/confeti/global/config/WebMvcConfig.java
+++ src/main/java/org/sopt/confeti/global/config/WebMvcConfig.java
@@ -7,6 +7,7 @@ import org.sopt.confeti.global.resolver.user.UserIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -22,5 +23,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToPerformanceTypeConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations("classpath:/static/docs/");
     }
 }
