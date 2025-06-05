ea347eeda48501c96d020b61416f331333acee25
chyun <och2510@gmail.com>
2025-06-05 15:16:28 +0900

    fix [#459] CORS에 docs url 추가

--- src/main/java/org/sopt/confeti/global/config/CorsConfig.java
+++ src/main/java/org/sopt/confeti/global/config/CorsConfig.java
@@ -37,6 +37,7 @@ public class CorsConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/docs/**", configuration);
 
         return source;
     }
