b7872255b3869137909fed0078c3da67f15493d4
ivoryeee <psa109@sookmyung.ac.kr>
2025-06-09 21:00:36 +0900

    refactor [#451] import 구문 와일드카드 제거

--- src/main/java/org/sopt/confeti/domain/artist_favorite/ArtistFavorite.java
+++ src/main/java/org/sopt/confeti/domain/artist_favorite/ArtistFavorite.java
@@ -1,7 +1,16 @@
 package org.sopt.confeti.domain.artist_favorite;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
--- src/main/java/org/sopt/confeti/domain/timetable_festival/TimetableFestival.java
+++ src/main/java/org/sopt/confeti/domain/timetable_festival/TimetableFestival.java
@@ -1,7 +1,17 @@
 package org.sopt.confeti.domain.timetable_festival;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AccessLevel;
