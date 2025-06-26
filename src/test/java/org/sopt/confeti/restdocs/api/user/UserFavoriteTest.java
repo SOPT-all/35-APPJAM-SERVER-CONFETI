5a1a5c434dae127ab5364815481f06976df5475b
chyun <och2510@gmail.com>
2025-06-26 23:17:35 +0900

    refactor [#482] RestAssured 테스트 개선
    * refactor [#482] 태그 추가를 위한 문서 스니펫 추가
    
    * refactor [#482] 테스트에 사용된 인증 헤더 값 숨김 처리
    
    * refactor [#482] 필드 값에 개행을 추가할 수 있도록 수정
    
    * refactor [#482] APIBaseTest 변경사항 반영
    
    * refactor [#482] APIBaseTest 변경사항 반영
    
    * feat [#479] 온보딩 여부 조회 API 구현
    
    * feat [#479] 온보딩 여부 조회 API 구현
    
    * docs [#479] openapi3.yaml 파일이 포함되지 않도록 gitignore 수정
    
    * refactor [#482] modifyUri로 수정한 값이 openapi3.yaml에 반영되지 않아 제거
    
    * etc [#482] 이거 왜 자꾸 생기지
    
    * refactor [#482] 태그 추가를 위한 문서 스니펫 추가
    
    * refactor [#482] 테스트에 사용된 인증 헤더 값 숨김 처리
    
    * refactor [#482] 필드 값에 개행을 추가할 수 있도록 수정
    
    * refactor [#482] APIBaseTest 변경사항 반영
    
    * refactor [#482] APIBaseTest 변경사항 반영
    
    * refactor [#482] modifyUri로 수정한 값이 openapi3.yaml에 반영되지 않아 제거

--- src/test/java/org/sopt/confeti/restdocs/api/user/UserFavoriteTest.java
+++ src/test/java/org/sopt/confeti/restdocs/api/user/UserFavoriteTest.java
@@ -1,13 +1,9 @@
 package org.sopt.confeti.restdocs.api.user;
 
-import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
 import static io.restassured.RestAssured.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,7 +14,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
-public class UserFavorite extends APIBaseTest {
+public class UserFavoriteTest extends APIBaseTest {
+
+    private static final String TAG = "user-favorite";
 
     @Test
     @DisplayName("아티스트 좋아요 삭제 API 테스트")
@@ -26,20 +24,21 @@ public class UserFavorite extends APIBaseTest {
         artistFavoriteRepository.save(ArtistFavorite.create(getUser(), TestDataManager.데이식스));
 
         given(this.spec)
-                .filter(document(DEFAULT_RESTDOC_PATH,
-                        requestHeaders(
-                                headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰")
-                        ),
-                        pathParameters(
-                                parameterWithName("artistId").description("아티스트 식별 ID")
-                        ),
-                        responseFields(
-                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("200"),
-                                fieldWithPath("message").type(JsonFieldType.STRING).description("요청이 성공했습니다.")
-                        )
+                .filter(APIDocument(
+                        tag(TAG)
+                                .requestHeaders(
+                                        headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰")
+                                )
+                                .pathParameters(
+                                        parameterWithName("artistId").description("아티스트 식별 ID")
+                                )
+                                .responseFields(
+                                        fieldWithPath("status").type(JsonFieldType.NUMBER).description("200"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING).description("요청이 성공했습니다.")
+                                )
                 ))
-                .accept(MediaType.APPLICATION_JSON_VALUE)
                 .when()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
                 .header(HttpHeaders.AUTHORIZATION, accessToken)
                 .pathParams("artistId", TestDataManager.데이식스)
                 .delete("/user/favorites/artists/{artistId}")
@@ -51,20 +50,21 @@ public class UserFavorite extends APIBaseTest {
     @DisplayName("아티스트 좋아요 추가 API 테스트")
     void 아티스트_좋아요_추가_API() {
         given(this.spec)
-                .filter(document(DEFAULT_RESTDOC_PATH,
-                        requestHeaders(
-                                headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰")
-                        ),
-                        pathParameters(
-                                parameterWithName("artistId").description("아티스트 식별 ID")
-                        ),
-                        responseFields(
-                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("200"),
-                                fieldWithPath("message").type(JsonFieldType.STRING).description("요청이 성공했습니다.")
-                        )
+                .filter(APIDocument(
+                        tag(TAG)
+                                .requestHeaders(
+                                        headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰")
+                                )
+                                .pathParameters(
+                                        parameterWithName("artistId").description("아티스트 식별 ID")
+                                )
+                                .responseFields(
+                                        fieldWithPath("status").type(JsonFieldType.NUMBER).description("200"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING).description("요청이 성공했습니다.")
+                                )
                 ))
-                .accept(MediaType.APPLICATION_JSON_VALUE)
                 .when()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
                 .header(HttpHeaders.AUTHORIZATION, accessToken)
                 .pathParams("artistId", TestDataManager.데이식스)
                 .post("/user/favorites/artists/{artistId}")
