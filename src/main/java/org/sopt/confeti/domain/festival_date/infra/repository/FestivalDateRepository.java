bea07545fe3840a4b17372b3c697ac23c1f39941
ivoryeee <psa109@sookmyung.ac.kr>
2025-06-26 19:10:46 +0900

    refactor [#486] 타임테이블 뷰 날짜 조회 API 개선: 종료된 페스티벌 조회 제외 조건 추가

--- src/main/java/org/sopt/confeti/domain/festival_date/infra/repository/FestivalDateRepository.java
+++ src/main/java/org/sopt/confeti/domain/festival_date/infra/repository/FestivalDateRepository.java
@@ -7,13 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface FestivalDateRepository extends JpaRepository<FestivalDate, Long> {
-    @Query("SELECT f " +
-            " FROM FestivalDate f " +
-            " JOIN f.stages s " +
-            " JOIN s.times t " +
-            " JOIN t.artists a " +
-            " WHERE f.id = :festivalDateId"
-    )
-    Optional<FestivalDate> findByFestivalDateId(
-            @Param("festivalDateId") long festivalDateId);
+    @Query("SELECT f FROM FestivalDate f JOIN f.stages s JOIN s.times t JOIN t.artists a WHERE f.id = :festivalDateId AND f.festival.endAt >= CURRENT_DATE")
+    Optional<FestivalDate> findByFestivalDateId(@Param("festivalDateId") long festivalDateId);
 }
