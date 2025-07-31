a3a8b95ef614c53ac593cc9d7cdf9832fecdea08
chyun <och2510@gmail.com>
2025-07-31 23:27:51 +0900

    refactor [#510] 공연 예매 링크 길이 추가

--- src/main/java/org/sopt/confeti/domain/festival_reservation_url/FestivalReservationUrl.java
+++ src/main/java/org/sopt/confeti/domain/festival_reservation_url/FestivalReservationUrl.java
@@ -32,7 +32,7 @@ public class FestivalReservationUrl {
     @JoinColumn(name = "festival_id")
     private Festival festival;
 
-    @Column(length = 100, nullable = false)
+    @Column(length = 200, nullable = false)
     private String reservationUrl;
 
     @Column(length = 50, nullable = false)
