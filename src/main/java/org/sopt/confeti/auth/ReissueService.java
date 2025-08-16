7a9e01241dc0dc4f68766553af7362d73d54b596
오치현 <choh@miridih.com>
2025-08-16 22:50:30 +0900

    refactor [#525] 토큰 내의 값을 새로 읽어오도록 수정

--- src/main/java/org/sopt/confeti/auth/ReissueService.java
+++ src/main/java/org/sopt/confeti/auth/ReissueService.java
@@ -8,6 +8,7 @@ import org.sopt.confeti.auth.jwt.TokenParser;
 import org.sopt.confeti.domain.token.RefreshToken;
 import org.sopt.confeti.domain.token.infra.RefreshTokenRepository;
 import org.sopt.confeti.domain.user.OAuthProvider;
+import org.sopt.confeti.domain.user.application.UserService;
 import org.sopt.confeti.domain.user.constant.Role;
 import org.sopt.confeti.global.exception.UnauthorizedException;
 import org.springframework.stereotype.Service;
@@ -21,12 +22,13 @@ public class ReissueService {
     private final JwtTokenValidator jwtTokenValidator;
     private final JwtTokenGenerator jwtTokenGenerator;
     private final JwtTokenExtractor jwtTokenExtractor;
+    private final UserService userService;
 
     @Transactional
     public Token reissue(String refreshToken) {
         RefreshToken validatedRefreshToken = getValidatedRefreshToken(refreshToken);
         String userId = jwtTokenExtractor.getSubject(validatedRefreshToken.getRefreshToken());
-        Role role = jwtTokenExtractor.getRole(validatedRefreshToken.getRefreshToken());
+        Role role = userService.getRole(Long.parseLong(userId));
         OAuthProvider provider = jwtTokenExtractor.getProvider(validatedRefreshToken.getRefreshToken());
 
         String newAccessToken = jwtTokenGenerator.createAccessToken(userId, role, provider);
