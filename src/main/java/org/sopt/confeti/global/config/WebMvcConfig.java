44fe1f9de4279d543ed7448934dd99f9b9b12a72
chyun <och2510@gmail.com>
2025-08-21 00:24:04 +0900

    SERVER-2 refactor: 레디스에 리프레시 토큰 저장 및 관리
    Signed-off-by: chyun <och2510@gmail.com>

--- src/main/java/org/sopt/confeti/global/config/WebMvcConfig.java
+++ src/main/java/org/sopt/confeti/global/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt.confeti.global.interceptor.CustomInterceptor;
 import org.sopt.confeti.global.converter.StringToPerformanceTypeConverter;
+import org.sopt.confeti.global.resolver.auth.TokenArgumentResolver;
 import org.sopt.confeti.global.resolver.auth.UserIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -18,11 +19,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final UserIdArgumentResolver userIdArgumentResolver;
+    private final TokenArgumentResolver tokenArgumentResolver;
     private final List<CustomInterceptor> customInterceptors;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userIdArgumentResolver);
+        resolvers.add(tokenArgumentResolver);
     }
 
     @Override
