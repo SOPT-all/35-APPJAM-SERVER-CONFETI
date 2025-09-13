ca9e956fc3993fe32a52e18b149b4d97e6cbb95e
김재헌 <127422627+jher235@users.noreply.github.com>
2025-09-13 18:15:19 +0900

    SERVER-21 elasticSearch 서버 환경 변화에 따른 config 수정 (#535)

--- src/main/java/org/sopt/confeti/global/config/ElasticSearchConfig.java
+++ src/main/java/org/sopt/confeti/global/config/ElasticSearchConfig.java
@@ -14,19 +14,12 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Value("${spring.data.elasticsearch.uris}")
     private String esHost;
 
-    @Value("${spring.data.elasticsearch.username}")
-    private String username;
-
-    @Value("${spring.data.elasticsearch.password}")
-    private String password;
-
     @NonNull
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo(esHost)
-                .usingSsl()
-                .withBasicAuth(username, password)
-                .build();
+            .connectedTo(esHost)
+            .usingSsl()
+            .build();
     }
 }
