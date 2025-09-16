3c3a6afa1a79a5a94765dd33ee55d8d128a1e5e8
jher235 <tim668666@gmail.com>
2025-09-16 18:54:26 +0900

    SERVER-24 feat: ArtistSong 엔티티 및 JpaRepository 생성

---
+++ src/main/java/org/sopt/confeti/domain/artist_song/ArtistSong.java
@@ -0,0 +1,52 @@
+package org.sopt.confeti.domain.artist_song;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.confeti.domain.artist.Artist;
+import org.sopt.confeti.domain.song.Song;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ArtistSong {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "artist_song_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "artist_id")
+    private Artist artist;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "song_id")
+    private Song song;
+
+    public static ArtistSong create(Long id, Artist artist, Song song) {
+        return ArtistSong.builder()
+            .id(id)
+            .artist(artist)
+            .song(song)
+            .build();
+    }
+
+    @Builder
+    private ArtistSong(Long id, Artist artist, Song song) {
+        this.id = id;
+        this.artist = artist;
+        this.song = song;
+    }
+}
---
+++ src/main/java/org/sopt/confeti/domain/artist_song/infra/ArtistSongRepository.java
@@ -0,0 +1,8 @@
+package org.sopt.confeti.domain.artist_song.infra;
+
+import org.sopt.confeti.domain.artist_song.ArtistSong;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistSongRepository extends JpaRepository<Long, ArtistSong> {
+
+}
