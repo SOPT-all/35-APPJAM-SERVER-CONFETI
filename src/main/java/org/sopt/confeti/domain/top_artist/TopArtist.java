cba210b22d681470cfc86c9170a8166d57cc4342
jher235 <tim668666@gmail.com>
2025-09-17 10:46:48 +0900

    SERVER-24 feat: 아이디를 인조 식별자로 변경 - rank를 id로 둬도 큰 문제는 없겠지만, 프로덕트의 다른 테이블들과 통일해서 사용할 수 있도록 인조식별자를 pk로 사용하도록 변경함

--- src/main/java/org/sopt/confeti/domain/top_artist/TopArtist.java
+++ src/main/java/org/sopt/confeti/domain/top_artist/TopArtist.java
@@ -3,6 +3,8 @@ package org.sopt.confeti.domain.top_artist;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -19,11 +21,15 @@ import org.sopt.confeti.domain.artist.Artist;
 public class TopArtist {
 
     @Id
-    @Column(name = "rank")
-    private Long rank;
+    @Column(name = "top_artist_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "artist_id")
     private Artist artist;
+
+    @Column(nullable = false)
+    private int rank;
 
 }
