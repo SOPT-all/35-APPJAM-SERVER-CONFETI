0c8525e22c0ec77051d2ec5e1e59589ecd97bf12
chyun <och2510@gmail.com>
2025-09-21 19:46:47 +0900

    CONFETI-24 feat: Apple Music Feign Client 구현

---
+++ src/main/java/org/sopt/confeti/external/client/AppleMusicFeignClient.java
@@ -0,0 +1,65 @@
+package org.sopt.confeti.external.client;
+
+import org.sopt.confeti.global.util.music.dto.artist.AppleMusicArtistResponse;
+import org.sopt.confeti.global.util.music.dto.artist.AppleMusicArtistsResponse;
+import org.sopt.confeti.global.util.music.dto.chart.AppleMusicChartsResponse;
+import org.sopt.confeti.global.util.music.dto.music.AppleMusicArtistMusicsResponse;
+import org.sopt.confeti.global.util.music.dto.music.AppleMusicMusicsResponse;
+import org.sopt.confeti.global.util.music.dto.search.AppleMusicSearchResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        name = "AppleMusicFeignClient",
+        url = "${apple-music.api.host}",
+        path = "${apple-music.api.path}",
+        configuration = AppleMusicFeignClient.class
+)
+public interface AppleMusicFeignClient {
+
+    @GetMapping("/artists/{id}")
+    AppleMusicArtistResponse getArtistById(
+            @PathVariable String id
+    );
+
+    @GetMapping("/artists")
+    AppleMusicArtistsResponse getArtists(
+            @RequestParam String ids
+    );
+
+    @GetMapping("/artists/{id}/view/{view}")
+    AppleMusicArtistsResponse getRelatedArtistsById(
+            @PathVariable String id,
+            @PathVariable String view,
+            @RequestParam String limit
+    );
+
+    @GetMapping("/artists/{id}/songs")
+    AppleMusicArtistMusicsResponse getArtistMusicsById(
+            @PathVariable String id,
+            @RequestParam String limit,
+            @RequestParam String offset
+    );
+
+    @GetMapping("/songs")
+    AppleMusicMusicsResponse getSongsByIds(
+            @RequestParam String ids
+    );
+
+    @GetMapping("/search")
+    AppleMusicSearchResponse searchByKeyword(
+            @RequestParam String term,
+            @RequestParam String types,
+            @RequestParam String limit,
+            @RequestParam String offset,
+            @RequestParam String with
+    );
+
+    @GetMapping("/charts")
+    AppleMusicChartsResponse getCharts(
+            @RequestParam String types,
+            @RequestParam String limit
+    );
+}
