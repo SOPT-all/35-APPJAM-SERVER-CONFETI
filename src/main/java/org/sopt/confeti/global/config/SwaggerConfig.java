f5374a1803794efd9ed33e2e69a85bb014dc9523
chyun <och2510@gmail.com>
2025-09-29 21:22:28 +0900

    CONFETI-27 feat: 스웨거 의존성 추가

--- src/main/java/org/sopt/confeti/global/config/SwaggerConfig.java
+++ src/main/java/org/sopt/confeti/global/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package org.sopt.confeti.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +12,77 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Info info = new Info()
-                .title("CONFETI Server API Document")
-                .version("v1.0.0")
-                .description("서버 API Swagger 명세서입니다.");
-
         return new OpenAPI()
                 .components(new Components())
-                .info(info);
+                .info(
+                        new Info()
+                                .title("CONFETI Server API Document")
+                                .version("v1.0.0")
+                                .description("서버 API Swagger 명세서입니다.")
+                );
+    }
+
+    /** 아티스트 그룹 */
+    @Bean
+    public GroupedOpenApi groupArtist() {
+        return GroupedOpenApi.builder()
+                .group("artist")
+                .pathsToMatch("/artists/**")
+                .build();
+    }
+
+    /** 어드민 그룹 */
+    @Bean
+    public GroupedOpenApi groupAdmin() {
+        return GroupedOpenApi.builder()
+                .group("admin")
+                .pathsToMatch("/admin/**")
+                .build();
+    }
+
+    /** 공연 그룹 */
+    @Bean
+    public GroupedOpenApi groupPerformance() {
+        return GroupedOpenApi.builder()
+                .group("performance")
+                .pathsToMatch("/performances/**")
+                .build();
+    }
+
+    /** 유저 그룹 */
+    @Bean
+    public GroupedOpenApi groupUser() {
+        return GroupedOpenApi.builder()
+                .group("user")
+                .pathsToMatch("/user/**")
+                .pathsToExclude("/user/onboard/**", "/user/timetables/**")
+                .build();
+    }
+
+    /** 인증 그룹 */
+    @Bean
+    public GroupedOpenApi groupAuth() {
+        return GroupedOpenApi.builder()
+                .group("auth")
+                .pathsToMatch("/auth/**", "/user/onboard/**")
+                .build();
+    }
+
+    /** 셋리스트 그룹 */
+    @Bean
+    public GroupedOpenApi groupSetlist() {
+        return GroupedOpenApi.builder()
+                .group("setlist")
+                .pathsToMatch("/my/setlists/**")
+                .build();
+    }
+
+    /** 유저 타임테이블 그룹 */
+    @Bean
+    public GroupedOpenApi groupUserTimetable() {
+        return GroupedOpenApi.builder()
+                .group("user timetable")
+                .pathsToMatch("/user/timetables/**")
+                .build();
     }
 }
