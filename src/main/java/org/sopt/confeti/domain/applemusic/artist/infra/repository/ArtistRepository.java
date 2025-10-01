c8c5d79f00e9f7bfa53626dc866546930be8381a
jher235 <tim668666@gmail.com>
2025-10-02 02:54:44 +0900

    CONFETI-28 feat: ArtistId 및 SongId로 객체의 존재 여부를 확인하는 메서드 추가

--- src/main/java/org/sopt/confeti/domain/applemusic/artist/infra/repository/ArtistRepository.java
+++ src/main/java/org/sopt/confeti/domain/applemusic/artist/infra/repository/ArtistRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArtistRepository extends JpaRepository<Artist, String> {
 
+    boolean existsByArtistId(String artistId);
 }
