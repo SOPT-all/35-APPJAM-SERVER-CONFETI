fccf0da5dc43c4527e3fe5a2721143a0f385a25d
jher235 <tim668666@gmail.com>
2025-10-02 03:11:53 +0900

    CONFETI-28 feat: 에러메세지 추가

--- src/main/java/org/sopt/confeti/global/message/ErrorMessage.java
+++ src/main/java/org/sopt/confeti/global/message/ErrorMessage.java
@@ -24,6 +24,7 @@ public enum ErrorMessage {
 
     /* 404 Not Found */
     NOT_FOUND(HttpStatus.NOT_FOUND, "요청하는 리소스가 존재하지 않습니다."),
+    EVENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이벤트 타입입니다."),
 
     /* 405 Method Not Allowed */
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "잘못된 HTTP Method 요청입니다."),
@@ -34,7 +35,8 @@ public enum ErrorMessage {
 
     /* 500 Internal Server Error*/
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
-    ;
+    FAIL_JSON_SERIALIZATION(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 직렬화에 실패했습니다."),
+    FAIL_JSON_DESERIALIZATION(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 역직렬화에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
