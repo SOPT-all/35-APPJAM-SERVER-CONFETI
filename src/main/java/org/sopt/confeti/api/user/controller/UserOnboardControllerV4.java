34a148a1906cef818b769051047e7a42c79c32e4
jher235 <tim668666@gmail.com>
2025-10-16 01:09:01 +0900

    CONFETI-39 feat: UserOnboardControllerV4 작성 - 온보딩 진행 중 favorite 으로 선택했던 아티스트 목록 조회 api 추가

---
+++ src/main/java/org/sopt/confeti/api/user/controller/UserOnboardControllerV4.java
@@ -0,0 +1,118 @@
+package org.sopt.confeti.api.user.controller;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import org.sopt.confeti.api.user.controller.docs.UserOnboardControllerV4Docs;
+import org.sopt.confeti.api.user.dto.response.UserOnboardTopArtistsResponse;
+import org.sopt.confeti.api.user.dto.response.onboard.GetOnboardStatusResponse;
+import org.sopt.confeti.api.user.dto.response.onboard.UserOnboardFavoriteArtistsResponse;
+import org.sopt.confeti.api.user.dto.response.onboard.UserOnboardRelatedArtistsResponse;
+import org.sopt.confeti.api.user.facade.UserOnboardFacade;
+import org.sopt.confeti.api.user.facade.dto.response.UserOnboardTopArtistsDTO;
+import org.sopt.confeti.api.user.facade.dto.response.onboard.GetOnboardStatusDTO;
+import org.sopt.confeti.api.user.facade.dto.response.onboard.UserOnboardFavoriteArtistsDTO;
+import org.sopt.confeti.api.user.facade.dto.response.onboard.UserOnboardRelatedArtistsDTO;
+import org.sopt.confeti.domain.user.constant.Role;
+import org.sopt.confeti.global.annotation.Permission;
+import org.sopt.confeti.global.annotation.UserId;
+import org.sopt.confeti.global.common.BaseResponse;
+import org.sopt.confeti.global.message.SuccessMessage;
+import org.sopt.confeti.global.util.ApiResponseUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user/onboard/v4")
+@Validated
+public class UserOnboardControllerV4 implements UserOnboardControllerV4Docs {
+
+    private final UserOnboardFacade userOnboardFacade;
+
+    /**
+     * 개발을 위해 임시로 Role.GENERAL 접근 허용
+     */
+    @Permission(role = {Role.ONBOARDING, Role.GENERAL})
+    @GetMapping("/artists/{artistId}/related")
+    public ResponseEntity<BaseResponse<UserOnboardRelatedArtistsResponse>> getRelatedArtists(
+        @UserId Long userId,
+        @PathVariable String artistId,
+        @RequestParam(defaultValue = "1") @Min(1) @Max(30) Integer limit
+    ) {
+        UserOnboardRelatedArtistsDTO relatedArtists = userOnboardFacade.getRelatedArtists(userId,
+            artistId, limit);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS,
+            UserOnboardRelatedArtistsResponse.from(relatedArtists));
+    }
+
+    @GetMapping("/artists/search")
+    public ResponseEntity<BaseResponse<UserOnboardRelatedArtistsResponse>> getArtistsRelatedTerm(
+        @UserId Long userId,
+        @RequestParam String term,
+        @RequestParam(defaultValue = "1") @Min(1) @Max(25) Integer limit
+    ) {
+        UserOnboardRelatedArtistsDTO relatedArtistsDTO = userOnboardFacade.getArtistsRelatedTerm(
+            userId, term, limit);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS,
+            UserOnboardRelatedArtistsResponse.from(relatedArtistsDTO));
+    }
+
+    /**
+     * 개발을 위해 임시로 Role.GENERAL 접근 허용
+     */
+    @Permission(role = {Role.ONBOARDING, Role.GENERAL})
+    @GetMapping("/artists")
+    public ResponseEntity<BaseResponse<UserOnboardTopArtistsResponse>> getTopArtists(
+        @UserId Long userId,
+        @RequestParam(required = false, defaultValue = "100") @Min(1) @Max(200) int limit
+    ) {
+        UserOnboardTopArtistsDTO topArtists = userOnboardFacade.getTopArtists(limit);
+        userOnboardFacade.cacheTopArtistsToUser(userId, topArtists);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS,
+            UserOnboardTopArtistsResponse.from(topArtists));
+    }
+
+    /**
+     * 개발을 위해 임시로 Role.GENERAL 접근 허용
+     */
+    @Permission(role = {Role.ONBOARDING, Role.GENERAL})
+    @PostMapping("/artists/{artistId}")
+    public ResponseEntity<BaseResponse<Void>> addArtist(
+        @UserId Long userId,
+        @PathVariable String artistId
+    ) {
+        userOnboardFacade.cacheTopArtist(userId, artistId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS);
+    }
+
+    @Permission(role = {Role.ONBOARDING, Role.GENERAL, Role.ADMIN})
+    @GetMapping("/status")
+    public ResponseEntity<BaseResponse<GetOnboardStatusResponse>> getOnboardStatus(
+        @UserId Long userId) {
+        GetOnboardStatusDTO onboardStatusDTO = userOnboardFacade.getOnboardStatus(userId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS,
+            GetOnboardStatusResponse.from(onboardStatusDTO));
+    }
+
+    /**
+     * 개발을 위해 임시로 Role.GENERAL 접근 허용
+     */
+    @Permission(role = {Role.ONBOARDING, Role.GENERAL})
+    @GetMapping("/artists/favorite")
+    public ResponseEntity<BaseResponse<UserOnboardFavoriteArtistsResponse>> getFavoriteArtists(
+        @UserId Long userId
+    ) {
+        UserOnboardFavoriteArtistsDTO favoriteArtists = userOnboardFacade.getFavoriteArtists(
+            userId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS,
+            UserOnboardFavoriteArtistsResponse.from(favoriteArtists));
+    }
+
+}
---
+++ src/main/java/org/sopt/confeti/api/user/controller/docs/UserOnboardControllerV4Docs.java
@@ -0,0 +1,29 @@
+package org.sopt.confeti.api.user.controller.docs;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.sopt.confeti.api.user.dto.response.onboard.UserOnboardFavoriteArtistsResponse;
+import org.sopt.confeti.global.common.BaseResponse;
+import org.sopt.confeti.global.common.swagger.CommonErrorResponses;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "유저 온보딩")
+public interface UserOnboardControllerV4Docs {
+
+    @Operation(summary = "온보딩 진행 중 favorite 으로 선택했던 아티스트 목록 조회")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "성공"
+            )
+        }
+    )
+    @CommonErrorResponses
+    ResponseEntity<BaseResponse<UserOnboardFavoriteArtistsResponse>> getFavoriteArtists(
+        Long userId
+    );
+
+}
