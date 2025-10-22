741eb52d6681b925f9115df538911dbaa4cdad2a
jher235 <tim668666@gmail.com>
2025-10-22 21:54:56 +0900

    CONFETI-33 feat: deleteFavoriteArtistIds 유효성 검사 추가

--- src/main/java/org/sopt/confeti/api/user/dto/request/PatchOnboardFavoriteArtistsRequest.java
+++ src/main/java/org/sopt/confeti/api/user/dto/request/PatchOnboardFavoriteArtistsRequest.java
@@ -1,12 +1,15 @@
 package org.sopt.confeti.api.user.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.Set;
 import org.sopt.confeti.api.user.facade.dto.request.PatchOnboardFavoriteArtistsDTO;
 
 public record PatchOnboardFavoriteArtistsRequest(
     @NotNull
-    Set<String> deleteFavoriteArtistIds
+    @Valid
+    Set<@NotBlank String> deleteFavoriteArtistIds
 ) {
 
     public PatchOnboardFavoriteArtistsDTO toDto() {
