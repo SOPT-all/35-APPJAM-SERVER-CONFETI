ea045ca6ffa5401bfea0ac4b64c7fd2b899aa3cb
jher235 <tim668666@gmail.com>
2025-11-13 21:59:13 +0900

    CONFETI-51 feat: SQS Listener Container 의 메세지 삭제 방식을 수동으로 변경

--- src/main/java/org/sopt/confeti/global/config/SqsConfig.java
+++ src/main/java/org/sopt/confeti/global/config/SqsConfig.java
@@ -4,6 +4,7 @@ import static org.sopt.confeti.global.config.ThreadPoolConfig.MESSAGE_CONSUMER_P
 import static org.sopt.confeti.global.config.ThreadPoolConfig.MESSAGE_PROVIDER_POOL;
 
 import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import java.time.Duration;
 import java.util.concurrent.Executor;
@@ -35,8 +36,12 @@ public class SqsConfig {
             .build();
     }
 
+    /**
+     * AcknowledgementMode.MANUAL: 폴링해온 메세지 목록의 삭제를 개별적으로 수행하기 위함 autoStartup(false): start 시점을
+     * 애플리케이션 실행 이후에 수동으로 지정
+     */
     @Bean
-    public SqsMessageListenerContainerFactory defaultSqsListenerContainerFactory(
+    public SqsMessageListenerContainerFactory createEventSqsListenerContainerFactory(
         SqsAsyncClient sqsAsyncClient,
         @Qualifier(MESSAGE_CONSUMER_POOL) TaskExecutor consumerExecutor
     ) {
@@ -44,6 +49,7 @@ public class SqsConfig {
             .builder()
             .configure(options -> options
                 .componentsTaskExecutor(consumerExecutor)
+                .acknowledgementMode(AcknowledgementMode.MANUAL)
                 .maxMessagesPerPoll(10)
                 .pollTimeout(Duration.ofSeconds(20))
                 .autoStartup(false))
