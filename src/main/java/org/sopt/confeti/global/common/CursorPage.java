346b9f82da0018a5c9e1fb00eeb2b61eeeb10b1f
chyun <och2510@gmail.com>
2025-11-19 00:48:57 +0900

    CONFETI-49 refactor: 커서 페이지 공통 DTO 문서 작성 및 조건 검사 수정

--- src/main/java/org/sopt/confeti/global/common/CursorPage.java
+++ src/main/java/org/sopt/confeti/global/common/CursorPage.java
@@ -4,20 +4,44 @@ import java.util.List;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Common DTO for Cursor-Based Paging Implementation
+ * @author chyun
+ * @param <T> T is the target to be retrieved using paging.
+ * itemsWithNextCursor is the retrieved list.
+ *
+ */
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class CursorPage<T> {
 
+    /**
+     * itemsWithNextCursor is the retrieved list.
+     */
     private final List<T> itemsWithNextCursor;
+
+    /**
+     * size is the number of items you attempted to retrieve.
+     * You must add one to the number of items required.
+     * For example, if you intend to respond with 10 items, you should retrieve 11 items, and the size value should be 11.
+     */
     private final int size;
 
     public static <T> CursorPage<T> of(final List<T> itemsWithNextCursor, final int size) {
         return new CursorPage<>(itemsWithNextCursor, size);
     }
 
+    /**
+     * Returns whether it is the last page
+     * @return boolean
+     */
     public boolean isLast() {
         return itemsWithNextCursor.size() < size;
     }
 
+    /**
+     * Returns the list of retrieved items.
+     * @return List of items of type T
+     */
     public List<T> getItems() {
         if (isLast()) {
             return itemsWithNextCursor;
@@ -26,7 +50,13 @@ public class CursorPage<T> {
         return itemsWithNextCursor.subList(0, size - 1);
     }
 
+    /**
+     * Returns the item pointed to by the next cursor.
+     * @return T
+     */
     public T getNextCursor() {
-        return itemsWithNextCursor.get(size - 1);
+        if (isLast()) return null;
+
+        return itemsWithNextCursor.getLast();
     }
 }
