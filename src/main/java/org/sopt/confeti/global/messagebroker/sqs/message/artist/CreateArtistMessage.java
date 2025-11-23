220c1c2e36d720141a75a88d8592e1192e0ff1df
chyun <och2510@gmail.com>
2025-11-23 20:57:53 +0900

    CONFETI-11 feat: 아티스트 생성 이벤트 추가

--- src/main/java/org/sopt/confeti/global/messagebroker/sqs/message/artist/CreateArtistMessage.java
+++ src/main/java/org/sopt/confeti/global/messagebroker/sqs/message/artist/CreateArtistMessage.java
@@ -1,6 +1,6 @@
 package org.sopt.confeti.global.messagebroker.sqs.message.artist;
 
-import org.sopt.confeti.domain.applemusic.artist.application.dto.request.CreateArtistDTO;
+import org.sopt.confeti.domain.music.artist.application.dto.request.CreateArtistDTO;
 import org.sopt.confeti.global.messagebroker.message.CreateMessage;
 
 public record CreateArtistMessage(
