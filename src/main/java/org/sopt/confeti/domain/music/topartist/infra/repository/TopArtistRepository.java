a01a80fea1d1dba9067ca52d1f4e8054e9ae55a2
chyun <och2510@gmail.com>
2025-11-23 20:55:14 +0900

    CONFETI-11 feat: 애플 뮤직 상위 아티스트 데이터 조회 캐싱 레이어 구현

--- src/main/java/org/sopt/confeti/domain/music/topartist/infra/repository/TopArtistRepository.java
+++ src/main/java/org/sopt/confeti/domain/music/topartist/infra/repository/TopArtistRepository.java
@@ -1,6 +1,6 @@
-package org.sopt.confeti.domain.applemusic.topartist.infra.repository;
+package org.sopt.confeti.domain.music.topartist.infra.repository;
 
-import org.sopt.confeti.domain.applemusic.topartist.TopArtist;
+import org.sopt.confeti.domain.music.topartist.TopArtist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopArtistRepository extends JpaRepository<TopArtist, Long> {
