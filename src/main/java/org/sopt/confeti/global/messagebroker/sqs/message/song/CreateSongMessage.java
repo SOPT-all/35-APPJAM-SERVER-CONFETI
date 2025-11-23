dd6d100cd636e531e5328fb0490c2b08657ec2dd
chyun <och2510@gmail.com>
2025-11-23 20:57:26 +0900

    CONFETI-11 feat: 노래 생성 이벤트 추가

--- src/main/java/org/sopt/confeti/global/messagebroker/sqs/message/song/CreateSongMessage.java
+++ src/main/java/org/sopt/confeti/global/messagebroker/sqs/message/song/CreateSongMessage.java
@@ -1,6 +1,6 @@
 package org.sopt.confeti.global.messagebroker.sqs.message.song;
 
-import org.sopt.confeti.domain.applemusic.song.application.dto.request.CreateSongDTO;
+import org.sopt.confeti.domain.music.song.application.dto.request.CreateSongDTO;
 import org.sopt.confeti.global.messagebroker.message.CreateMessage;
 
 public record CreateSongMessage(
