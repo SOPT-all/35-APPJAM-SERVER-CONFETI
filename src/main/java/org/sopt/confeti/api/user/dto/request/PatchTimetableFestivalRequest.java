2a7d88f8176194c39705b04dda58df4ecb745142
jher235 <tim668666@gmail.com>
2025-11-26 01:08:31 +0900

    CONFETI-58 feat: dto 필드를 timetableId 목록으로 수정

--- src/main/java/org/sopt/confeti/api/user/dto/request/PatchTimetableFestivalRequest.java
+++ src/main/java/org/sopt/confeti/api/user/dto/request/PatchTimetableFestivalRequest.java
@@ -3,11 +3,15 @@ package org.sopt.confeti.api.user.dto.request;
 import java.util.Set;
 import org.sopt.confeti.api.user.facade.dto.request.PatchTimetableFestivalDTO;
 
+/**
+ * 타임 테이블 삭제를 위함.
+ * TODO 관련 네이밍 재정의
+ */
 public record PatchTimetableFestivalRequest(
-    Set<Long> deleteFestivalIds
+    Set<Long> deleteTimetableFestivalIds
 ) {
 
     public PatchTimetableFestivalDTO toDTO() {
-        return new PatchTimetableFestivalDTO(deleteFestivalIds);
+        return new PatchTimetableFestivalDTO(deleteTimetableFestivalIds);
     }
 }
--- src/main/java/org/sopt/confeti/api/user/facade/dto/request/PatchTimetableFestivalDTO.java
+++ src/main/java/org/sopt/confeti/api/user/facade/dto/request/PatchTimetableFestivalDTO.java
@@ -3,7 +3,7 @@ package org.sopt.confeti.api.user.facade.dto.request;
 import java.util.Set;
 
 public record PatchTimetableFestivalDTO(
-    Set<Long> deleteFestivalIds
+    Set<Long> deleteTimetableIds
 ) {
 
 }
