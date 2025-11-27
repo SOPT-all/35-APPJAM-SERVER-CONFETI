1feef15600a0c7d3d540a03b77c8288a3b9ec004
chyun <och2510@gmail.com>
2025-11-27 22:12:41 +0900

    CONFETI-59 refactor: 이전 로직 Deprecated 처리

--- src/main/java/org/sopt/confeti/api/setlist/controller/SetlistControllerV4.java
+++ src/main/java/org/sopt/confeti/api/setlist/controller/SetlistControllerV4.java
@@ -25,11 +25,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Deprecated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/my/setlists")
-public class SetlistController {
+@RequestMapping("/v4/my/setlists")
+public class SetlistControllerV4 {
 
     private final SetlistFacade setlistFacade;
 
