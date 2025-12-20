5d8bfff3f0208ce3d580a5c292ccf861b6778c03
chyun <och2510@gmail.com>
2025-12-20 16:25:18 +0900

    CONFETI-72 ApiVersion 어노테이션 추가 및 버전 값 검증 로직 추가

---
+++ src/main/java/org/sopt/confeti/global/annotation/ApiVersionValidator.java
@@ -0,0 +1,68 @@
+package org.sopt.confeti.global.annotation;
+
+import java.lang.reflect.Method;
+import java.util.regex.Pattern;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+/**
+ * Verify that the version value is correctly set when @ApiVersion is specified.
+ */
+@Component
+public class ApiVersionValidator implements ApplicationRunner {
+
+    private static final String versionRegex = "^v[1-9]\\d*$";
+    private static final Pattern versionPattern = Pattern.compile(versionRegex);
+
+    private final RequestMappingHandlerMapping handlerMapping;
+
+    public ApiVersionValidator(
+        @Qualifier("requestMappingHandlerMapping") RequestMappingHandlerMapping handlerMapping) {
+        this.handlerMapping = handlerMapping;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        handlerMapping.getHandlerMethods().forEach((requestMappingInfo, handlerMethod) -> {
+            validateApiVersion(handlerMethod);
+        });
+    }
+
+    private void validateApiVersion(HandlerMethod handlerMethod) {
+        Method method = handlerMethod.getMethod();
+        ApiVersion apiVersion = method.getAnnotation(ApiVersion.class);
+
+        if (apiVersion != null) {
+            validateVersion(apiVersion.value(), method.getDeclaringClass().getSimpleName(),
+                method.getName());
+        }
+    }
+
+    private void validateVersion(String version, String className, String methodName) {
+        try {
+            validateNotBlank(version);
+            validateFormat(version);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(
+                String.format("%s [%s.%s]", e.getMessage(), className, methodName));
+        }
+    }
+
+    private void validateNotBlank(String version) {
+        if (!StringUtils.hasText(version)) {
+            throw new IllegalStateException("@ApiVersion의 version 값은 필수입니다.");
+        }
+    }
+
+    private void validateFormat(String version) {
+        if (!versionPattern.matcher(version).matches()) {
+            throw new IllegalStateException(
+                "@ApiVersion의 version 값이 올바르지 않습니다. 정규식 (" + versionRegex + ")를 따라야 합니다.");
+        }
+    }
+}
