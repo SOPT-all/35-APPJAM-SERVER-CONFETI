8cfe0c7eae2d28a8d90d43b4d74e20a660bf823f
chyun <och2510@gmail.com>
2025-12-23 16:42:29 +0900

    CONFETI-74 feat: UserRepository findById 에 Transactional 설정 제거

--- src/main/java/org/sopt/confeti/domain/user/infra/repository/UserRepository.java
+++ src/main/java/org/sopt/confeti/domain/user/infra/repository/UserRepository.java
@@ -1,18 +1,26 @@
 package org.sopt.confeti.domain.user.infra.repository;
 
 import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
 import org.sopt.confeti.domain.user.OAuthProvider;
 import org.sopt.confeti.domain.user.User;
+import org.sopt.confeti.global.annotation.ReadOnlyTransactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @NotNull
+    @Override
+    @ReadOnlyTransactional
+    Optional<User> findById(@NotNull Long userId);
+
     @Query(value =
-            "SELECT DISTINCT u" +
-                    " FROM User u" +
-                    " LEFT JOIN FETCH u.timetableFestivals tf" +
-                    " WHERE u.id = :userId"
+        "SELECT DISTINCT u" +
+            " FROM User u" +
+            " LEFT JOIN FETCH u.timetableFestivals tf" +
+            " WHERE u.id = :userId"
     )
     Optional<User> findUserTimetablesById(final @Param("userId") long userId);
 
---
+++ src/main/java/org/sopt/confeti/global/annotation/ReadOnlyTransactional.java
@@ -0,0 +1,15 @@
+package org.sopt.confeti.global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+public @interface ReadOnlyTransactional {
+
+}
