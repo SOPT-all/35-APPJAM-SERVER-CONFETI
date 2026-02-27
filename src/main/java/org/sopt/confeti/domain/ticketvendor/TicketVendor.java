529e54211e0b0554dd3336ec232ea8abdffdbb43
jher235 <tim668666@gmail.com>
2026-02-27 13:23:03 +0900

    CSERVER-9 feat: createdAt, updatedAt 필드 추가

--- src/main/java/org/sopt/confeti/domain/ticketvendor/TicketVendor.java
+++ src/main/java/org/sopt/confeti/domain/ticketvendor/TicketVendor.java
@@ -1,7 +1,14 @@
 package org.sopt.confeti.domain.ticketvendor;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "ticket_vendors")
 public class TicketVendor {
 
@@ -26,6 +34,13 @@ public class TicketVendor {
 
     @Column(length = 250, nullable = false)
     private String logoPath;
+    
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     @Builder
     public TicketVendor(String name, String logoPath) {
