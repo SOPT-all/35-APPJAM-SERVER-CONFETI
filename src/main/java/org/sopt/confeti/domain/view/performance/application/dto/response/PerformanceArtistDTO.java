428a41be576e7c38393a49fc1fdff80c11d09f64
chyun <och2510@gmail.com>
2026-03-22 21:04:57 +0900

    CSERVER-41 refactor: 페스티벌 등록 시 검색 엔진 업로드 이벤트 NPE 오류 수정

--- src/main/java/org/sopt/confeti/domain/view/performance/application/dto/response/PerformanceArtistDTO.java
+++ src/main/java/org/sopt/confeti/domain/view/performance/application/dto/response/PerformanceArtistDTO.java
@@ -3,13 +3,14 @@ package org.sopt.confeti.domain.view.performance.application.dto.response;
 import org.sopt.confeti.domain.view.performance.PerformanceArtist;
 
 public record PerformanceArtistDTO(
-        long id,
-        String artistId
+    Long id,
+    String artistId
 ) {
+
     public static PerformanceArtistDTO from(PerformanceArtist artist) {
         return new PerformanceArtistDTO(
-                artist.getId(),
-                artist.getArtistId()
+            artist.getId(),
+            artist.getArtistId()
         );
     }
 }
